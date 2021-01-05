1b0eea9118b5964353b753e8f41aeca15c95c737
Yewon Lee <yewon.lee@coinbase.com>
2021-01-05 15:12:22 -0800

    [CI-2294] Adding skeleton shimmer component (#9416)
    * [CI-2294] Adding skeleton shimmer component
    
    What changed:
     - moving loading skeleton to designSystem folder
     - assigning default values for shimmer color
     - adding css.d.ts to handle css variable on styles
     - removing header from skeletonTable

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,5 +4,11 @@ module.exports = {
   rules: {
     'plugin/no-low-performance-animation-properties': true,
     'plugin/no-unsupported-browser-features': true,
+    'number-max-precision': [
+      0,
+      {
+        ignoreUnits: ['em', 'rem', 's'],
+      },
+    ],
   },
 };
