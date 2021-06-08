6f222dd53fd384bb56edb868bdc751858331395e
Ky Lee <ky.lee@coinbase.com>
2021-06-08 17:23:02 -0400

    [trivial] update babel configs for react-native (#22425)
    * move module:metro-react-native-babel-preset to root babel config
    * remove @babel/plugin-transform-react-jsx
    * update Podfile.lock file

--- mobile-playground/.babelrc.js
+++ mobile-playground/.babelrc.js
@@ -3,9 +3,5 @@ const rootBabelConfig = require('../../../../babel.config');
 module.exports = api => {
   const config = rootBabelConfig(api);
 
-  return {
-    ...config,
-    plugins: [...config.plugins, ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]],
-    presets: ['module:metro-react-native-babel-preset', ...config.presets],
-  };
+  return config;
 };
