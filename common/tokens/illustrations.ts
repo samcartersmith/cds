7e81aeea2b0d08f1aa8fb57dc19ed38416d9328d
Jennifer Liu <jennifer.liu@coinbase.com>
2021-06-24 15:05:38 -0700

    Add illustrationSizes token. (#24591)
    * Add illustrationSizes token.

---
+++ common/tokens/illustrations.ts
@@ -0,0 +1,18 @@
+export const illustrationSizes = {
+  heroSquare: {
+    width: 240,
+    height: 240,
+  },
+  spotRectangle: {
+    width: 240,
+    height: 120,
+  },
+  spotSquare: {
+    width: 120,
+    height: 120,
+  },
+  pictogram: {
+    width: 64,
+    height: 64,
+  },
+};
