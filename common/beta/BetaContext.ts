cd86e895f78fe36c15d54b44a258912646c8f75a
Travis Ueki <travis.ueki@coinbase.com>
2021-09-07 14:09:12 -0500

    [trivial] chore: Type change, remove superfluous type (#32336)
    * [trivial] chore: Type change, remove superfluous type

--- common/beta/BetaContext.ts
+++ common/beta/BetaContext.ts
@@ -1,6 +1,5 @@
 import { createContext } from 'react';
 
 export const DEFAULT_CONTEXT_VALUE = { fontMigration: false };
-export type BetaContextProps = Record<string, boolean>;
 export type BetaContextType = typeof DEFAULT_CONTEXT_VALUE;
-export const BetaContext = createContext<BetaContextProps>(DEFAULT_CONTEXT_VALUE);
+export const BetaContext = createContext<BetaContextType>(DEFAULT_CONTEXT_VALUE);
