dd89b33e2cb04b017813d087c8a9c8c731af9fb0
Katherine Martinez <katherine.martinez@coinbase.com>
2021-12-06 17:41:01 -0500

    [trivial] feat(CardGroup): frontier gated CardGroup updates for mobile (#44720)
    * [trivial] feat(CardGroup): frontier gated CardGroup updates
    
    * fix prettier error

--- mobile/cards/CardGroup.tsx
+++ mobile/cards/CardGroup.tsx
@@ -1,11 +1,14 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 
-import { CardGroupBaseProps, useBeta } from '@cbhq/cds-common';
 import { View, StyleSheet } from 'react-native';
+import { CardGroupBaseProps } from '@cbhq/cds-common';
 import flattenNodes from '@cbhq/cds-common/utils/flattenNodes';
+import { join } from '@cbhq/cds-common/utils/join';
+import { Divider } from '../layout/Divider';
 import { HStack } from '../layout/HStack';
 import { VStack } from '../layout/VStack';
 import { Card } from './Card';
+import { useFeatureFlag } from '../system/useFeatureFlag';
 
 export const CardGroup = memo(function CardGroup({
   accessibilityLabel,
@@ -14,8 +17,21 @@ export const CardGroup = memo(function CardGroup({
   horizontal,
   ...otherBoxProps
 }: CardGroupBaseProps) {
-  const { frontierCard } = useBeta();
+  const isFrontier = useFeatureFlag('frontierCard');
   const Stack = horizontal ? HStack : VStack;
+
+  const cards = useMemo(() => {
+    const nodes = flattenNodes(children)
+      .filter((child) => child && typeof child === 'object' && child.type === Card)
+      .map((child) => {
+        return <View style={styles.cardItem}>{child}</View>;
+      });
+
+    if (isFrontier) {
+      return join(nodes, <Divider />);
+    }
+    return nodes;
+  }, [children, isFrontier]);
 
   return (
     <Stack
@@ -23,15 +39,11 @@ export const CardGroup = memo(function CardGroup({
       accessibilityLabel={accessibilityLabel}
       alignItems="stretch"
       flexWrap="nowrap"
-      gap={frontierCard ? 0 : 1}
+      gap={isFrontier ? 0 : 1}
       testID={testID}
       {...otherBoxProps}
     >
-      {flattenNodes(children)
-        .filter((child) => child && typeof child === 'object' && child.type === Card)
-        .map((child) => {
-          return <View style={styles.cardItem}>{child}</View>;
-        })}
+      {cards}
     </Stack>
   );
 });
