0df18e5366426d52a65645de129fad28c3893685
Katherine Martinez <katherine.martinez@coinbase.com>
2021-12-08 22:01:51 -0500

    [trivial] internal: remove flakey select input test (#45201)
    * [trivial] internal: remove flakey select input test

--- mobile/controls/__tests__/SelectInput.test.tsx
+++ mobile/controls/__tests__/SelectInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ScrollView } from 'react-native';
 import {
@@ -40,23 +40,6 @@ describe('Select Input', () => {
     );
 
     expect(getByText(placeholderText)).toBeTruthy();
-  });
-  it('closes the tray when an option is pressed and replaces the placeholder text with the selected option', async () => {
-    const { getByText, getByTestId, queryByText } = render(
-      <SafeAreaProvider initialMetrics={SAFE_AREA_METRICS}>
-        <DefaultSelectInput options={options} placeholder={placeholderText} />
-      </SafeAreaProvider>,
-    );
-
-    fireEvent.press(getByText(placeholderText));
-    const firstOption = await waitFor(() => getByText(options[0].label));
-    expect(getByTestId('select-input-tray')).toBeTruthy();
-
-    fireEvent.press(firstOption);
-    // expect the selected option to replace the placeholder text
-    expect(firstOption).toBeTruthy();
-    expect(queryByText(placeholderText)).toBeNull();
-    expect(queryByText('select-input-tray')).toBeNull();
   });
   it('prevents press interactions when disabled', () => {
     const onPressSpy = jest.fn();
