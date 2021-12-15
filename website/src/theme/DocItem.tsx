15adb1de6f1899a078329d1b08f6ce41f9abe835
Jake LeBoeuf <jake.leboeuf@coinbase.com>
2021-12-15 15:05:40 -0500

    [trivial] docs: Update CDS docsite to use CDS typo elements (#46301)
    * Update docs to use CDS typo elements
    
    * Move MDXProvder into it's own component so we can use it on the sidebars one day
    
    * Match title setup to the latest docs figma
    
    * Adds types for @mdx-js/react

--- website/src/theme/DocItem.tsx
+++ website/src/theme/DocItem.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import OriginalDocItem from '@theme-original/DocItem';
+import { TextDisplay3 } from '@cbhq/cds-web/typography';
+import { MDXProvider } from ':cds-website/components/MDXProvider';
 
 // This file forces our right sidebar to render by tricking
 // Docusaurus into thinking we have a TOC.
-
 export default function DocItem({
   content,
   ...props
 }: {
-  content: { toc: { id: string; value: string; children: unknown[] }[] };
+  content: {
+    frontMatter: { title?: string; hide_title?: boolean };
+    toc: { id: string; value: string; children: unknown[] }[];
+  };
 }) {
-  content.toc.push({
-    id: '',
-    value: '',
-    children: [],
-  });
+  // eslint-disable-next-line no-param-reassign
+  content.frontMatter.hide_title = true;
+  content.toc.push({ id: '', value: '', children: [] });
 
-  return <OriginalDocItem {...props} content={content} />;
+  return (
+    <MDXProvider>
+      <TextDisplay3 as="h1" spacingBottom={2}>
+        {content.frontMatter.title}
+      </TextDisplay3>
+      <OriginalDocItem {...props} content={content} />
+    </MDXProvider>
+  );
 }
