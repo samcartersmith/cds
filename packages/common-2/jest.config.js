03444744c81399ec0615e14c8212692a5e76f000
Jonathan Rossi <jonathan.rossi@coinbase.com>
2022-01-19 20:52:51 -0800

    Add common-2 missing files

---
+++ packages/common-2/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'common-2',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/common-2',
+};
