3830d8830a416dcffc695fda14d5c1e56409d841
Jonathan Rossi <jonathan.rossi@coinbase.com>
2022-01-22 20:02:37 -0800

    Config cleanup
    - Added nvmrc
    - Updated npmrc to our config
    - Added engine to package.json

---
+++ tools/ci/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "---- Installing yarn dependencies"
+
+# Disable global cache so that we can cache `.yarn/cache` in buildkite
+yarn config set enableGlobalCache false
+
+# Immutable is the same as a frozen lockfile
+yarn install --immutable
+
+echo "---- Building required packages"
+
+yarn build
+
+echo "---- Setup complete, running jobs"
