293c682a0e8eb9d9819e4bc49ee4628ae0a434a9
JenniferLiu <jennifer.liu@coinbase.com>
2022-02-14 22:28:31 -0800

    Moved babel config from mobile to mobile-playground.

--- apps/mobile-playground/babel.config.js
+++ apps/mobile-playground/babel.config.js
@@ -8,4 +8,21 @@ module.exports = {
     'module:metro-react-native-babel-preset',
     ['linaria/babel', { classNameSlug: (hash, title) => (isTestEnv ? title : `${title}-${hash}`) }],
   ],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./'],
+        alias: {
+          '@cbhq/cds-common': '../../packages/common',
+          '@cbhq/cds-utils': '../../packages/utils',
+          '@cbhq/cds-lottie-files': '../../packages/lottie-files',
+          '@cbhq/cds-fonts': '../../packages/fonts',
+          '@cbhq/cds-web-utils': '../../packages/web-utils',
+          '@cbhq/cds-web': '../../packages/web',
+          ':cds-website': './',
+        },
+      },
+    ],
+  ],
 };
--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -4,20 +4,4 @@ module.exports = {
     ['@babel/preset-react', { runtime: 'automatic' }],
     '@babel/preset-typescript',
   ],
-  plugins: [
-    [
-      require.resolve('babel-plugin-module-resolver'),
-      {
-        root: ['./'],
-        alias: {
-          '@cbhq/cds-common': '../common',
-          '@cbhq/cds-utils': '../utils',
-          '@cbhq/cds-lottie-files': '../lottie-files',
-          '@cbhq/cds-fonts': '../fonts',
-          '@cbhq/cds-web-utils': '../web-utils',
-          '@cbhq/cds-web': '../web',
-        },
-      },
-    ],
-  ],
 };
