204390ecdf37747c2cb7ce963e6a581be416663e
Katherine Martinez <katherine.martinez@coinbase.com>
2022-02-17 12:54:49 -0500

    [CDS-2085] feat: ProgressIndicator for new Carousel (#55131)
    * [CDS-2085} feat: ProgressIndicator for new Carousel
    
    * include CarouselControlsWrapper

---
+++ mobile/media/Carousel/CarouselControlsWrapper.tsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import { zIndex } from '@cbhq/cds-common/tokens/zIndex';
+import { gutter } from '@cbhq/cds-common/tokens/sizing';
+import { HStack, HStackProps } from '../../layout/HStack';
+
+export type CarouselControlsWrapperProps = HStackProps & {
+  children?: React.ReactNode;
+};
+
+/** Wrapper to house ProgressIndicators and Dismiss IconButton when controls are shown in Carousel */
+export const CarouselControlsWrapper = memo(function CarouselControlsWrapper({
+  children,
+  opacity,
+  ...props
+}: CarouselControlsWrapperProps) {
+  return (
+    <HStack
+      position="absolute"
+      top={0}
+      left={0}
+      right={0}
+      spacingHorizontal={gutter}
+      alignItems="center"
+      zIndex={zIndex.interactable}
+      animated
+      opacity={opacity}
+      {...props}
+    >
+      {children}
+    </HStack>
+  );
+});
+
+CarouselControlsWrapper.displayName = 'CarouselControlsWrapper';
---
+++ mobile/visualizations/ProgressIndicator.tsx
@@ -0,0 +1,73 @@
+import React, { forwardRef, memo, useMemo } from 'react';
+import { Animated, StyleSheet, StyleProp, View, ViewStyle } from 'react-native';
+import { useInteractableHeight } from '@cbhq/cds-common/hooks/useInteractableHeight';
+import { Box, BoxProps } from '../layout/Box';
+import { usePalette } from '../hooks/usePalette';
+
+export const PROGRESS_INDICATOR_WIDTH = 24;
+
+export type ProgressIndicatorProps = {
+  /** An Animated.Value or interpolated Animated.Value between 0 and 1 */
+  progress?: Animated.Value | Animated.AnimatedInterpolation;
+  /**
+   * @danger This is a migration escape hatch. It is not intended to be used normally.
+   */
+  dangerouslySetStyle?: Animated.WithAnimatedValue<StyleProp<ViewStyle>>;
+} & BoxProps;
+
+export const ProgressIndicator = memo(
+  forwardRef<View, ProgressIndicatorProps>(function ProgressIndicator(
+    { progress, dangerouslySetStyle, ...boxProps },
+    ref,
+  ) {
+    const { line, foreground } = usePalette();
+    const wrapperHeight = useInteractableHeight(true);
+    const outerStyles = useMemo(() => [styles.dash, { backgroundColor: line }], [line]);
+    const innerStyles = useMemo(
+      () => [
+        styles.dashOverlay,
+        { backgroundColor: foreground, zIndex: 2 },
+        progress && {
+          transform: [
+            {
+              translateX: progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [-PROGRESS_INDICATOR_WIDTH, 0],
+              }),
+            },
+          ],
+        },
+      ],
+      [foreground, progress],
+    );
+
+    return (
+      <Box
+        ref={ref}
+        animated
+        height={wrapperHeight}
+        alignItems="center"
+        justifyContent="center"
+        dangerouslySetStyle={dangerouslySetStyle}
+        {...boxProps}
+      >
+        <Animated.View style={outerStyles}>
+          <Animated.View style={innerStyles} />
+        </Animated.View>
+      </Box>
+    );
+  }),
+);
+
+const styles = StyleSheet.create({
+  dash: {
+    height: 2,
+    width: PROGRESS_INDICATOR_WIDTH,
+    borderRadius: 99,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  dashOverlay: StyleSheet.absoluteFillObject,
+});
+
+ProgressIndicator.displayName = 'ProgressIndicator';
