88cf69036629e1c149c60aab1ae72b79fc311824
Jennifer Liu <jennifer.liu@coinbase.com>
2022-02-17 17:11:19 -0800

    [trivial] fix(Link): Remove url-parse from cds-mobile dependency. (#55321)
    * Remove url-parse from cds-mobile dependency.
    
    * Fix https and http issue.

--- mobile/hooks/useWebBrowserOpener.ts
+++ mobile/hooks/useWebBrowserOpener.ts
@@ -4,42 +4,35 @@ import { useSpectrum } from '@cbhq/cds-common';
 import { Linking } from 'react-native';
 
 import { openWebBrowser, OpenWebBrowserOptions } from '../utils/openWebBrowser';
-import { parseUrl } from '../utils/urlUtil';
 
 export const useWebBrowserOpener = () => {
   const spectrum = useSpectrum();
   return useCallback(
     async (url: string, options?: Partial<OpenWebBrowserOptions>) => {
-      const parsedUrl = parseUrl(url);
+      if (url.startsWith('https://') || url.startsWith('http://')) {
+        // Use custom handling for web URLs
+        await openWebBrowser(url, {
+          spectrum: options?.spectrum ?? spectrum,
+          ...options,
+        });
+        return;
+      }
 
-      switch (parsedUrl.protocol) {
-        case 'http':
-        case 'https':
-          // Use custom handling for web URLs
-          await openWebBrowser(url, {
-            spectrum: options?.spectrum ?? spectrum,
-            ...options,
-          });
-          break;
-        default: {
-          // Handle other schemes such as mailto, tel, and sms
-          try {
-            const canOpenUrl = await Linking.canOpenURL(url);
+      // Handle other schemes such as mailto, tel, and sms
+      try {
+        const canOpenUrl = await Linking.canOpenURL(url);
 
-            if (!canOpenUrl) {
-              // TODO: replace these with Bugsnag one day
-              // eslint-disable-next-line no-console
-              console.error(`Cannot open URL ${url}`);
-              return;
-            }
-
-            await Linking.openURL(url);
-          } catch (err) {
-            // eslint-disable-next-line no-console
-            console.error(err);
-          }
-          break;
+        if (!canOpenUrl) {
+          // TODO: replace these with Bugsnag one day
+          // eslint-disable-next-line no-console
+          console.error(`Cannot open URL ${url}`);
+          return;
         }
+
+        await Linking.openURL(url);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
       }
     },
     [spectrum],
