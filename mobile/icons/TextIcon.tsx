6a396eea92b8fc7f91afd85de0637f4254ffb302
Katherine Martinez <katherine.martinez@coinbase.com>
2022-02-22 14:06:00 -0500

    [CDS-2115] update(TextIcon): add animated and dangerouslySetStyle props (#55691)
    * [CDS-2115] update(TextIcon): add animated and dangerouslySetStyle props
    
    * fix types to ensure animated styles are gated by animated prop

--- mobile/icons/TextIcon.tsx
+++ mobile/icons/TextIcon.tsx
@@ -1,14 +1,23 @@
-import React, { useMemo } from 'react';
-
+import React, { memo, useMemo } from 'react';
+import { Animated, StyleProp, Text, TextStyle } from 'react-native';
 import { PaletteForeground } from '@cbhq/cds-common';
 import { useIconSize } from '@cbhq/cds-common/hooks/useIconSize';
-import { Text } from 'react-native';
 
 import { usePalette } from '../hooks/usePalette';
 import { IconProps } from './Icon';
 import { iconGlyphMap } from './iconGlyphMap';
 
-export type TextIconProps = Pick<IconProps, 'color' | 'size' | 'name' | 'testID'>;
+export type TextIconProps = Pick<IconProps, 'color' | 'size' | 'name' | 'testID'> &
+  (
+    | {
+        animated: true;
+        dangerouslySetStyle: Animated.WithAnimatedValue<StyleProp<TextStyle>>;
+      }
+    | {
+        animated?: false | undefined;
+        dangerouslySetStyle?: StyleProp<TextStyle>;
+      }
+  );
 
 /**
  *
@@ -16,30 +25,37 @@ export type TextIconProps = Pick<IconProps, 'color' | 'size' | 'name' | 'testID'
  * Nested text components need to all be RN Text and the Icon component has a box wrapper to handle border and badges.
  * Adheres to all the requirements for Nested Text on react native. https://reactnative.dev/docs/text#nested-text
  */
-export const TextIcon = ({
+export const TextIcon = memo(function TextIcon({
+  animated,
   color = 'primary' as PaletteForeground,
   size,
   name,
   testID,
-}: TextIconProps) => {
+  dangerouslySetStyle,
+}: TextIconProps) {
+  const Component = animated ? Animated.Text : Text;
   const { iconSize } = useIconSize(size, false);
   const iconColor = usePalette()[color];
 
   const styles = useMemo(
-    () => ({
-      fontFamily: 'CoinbaseIcons',
-      fontSize: iconSize,
-      color: iconColor,
-    }),
-    [iconColor, iconSize],
+    () =>
+      [
+        {
+          fontFamily: 'CoinbaseIcons',
+          fontSize: iconSize,
+          color: iconColor,
+        },
+        dangerouslySetStyle,
+      ] as TextStyle,
+    [dangerouslySetStyle, iconColor, iconSize],
   );
 
-  if (name in iconGlyphMap && iconSize in iconGlyphMap[name]) {
+  if (iconGlyphMap[name][iconSize]) {
     return (
-      <Text testID={testID} allowFontScaling={false} style={styles}>
+      <Component testID={testID} allowFontScaling={false} accessibilityRole="image" style={styles}>
         {iconGlyphMap[name][iconSize]}
-      </Text>
+      </Component>
     );
   }
   return <>{null}</>;
-};
+});
