40355da3ce5bad8516c9de4967cd056bdf318a6b
Jonathan Rossi <jonathan.rossi@coinbase.com>
2022-03-10 11:14:51 -0600

    [trivial] internal: Deleted website

--- apps/website/components/AdoptionTracker/hooks/useAdoptionPercent.ts
+++
@@ -1,9 +0,0 @@
-import { getPercentageText } from '../utils/getPercentageText';
-
-import { useAdopterComponents } from './useAdopterComponents';
-
-export function useAdoptionPercent(variant: 'cds' | 'presentational') {
-  const groups = useAdopterComponents();
-  const totalInstances = groups.cds.totalInstances + groups.presentational.totalInstances;
-  return getPercentageText(groups[variant].totalInstances, totalInstances);
-}
--- apps/website/src/theme/Root.tsx
+++
@@ -1,9 +0,0 @@
-import BrowserOnly from '@docusaurus/BrowserOnly';
-
-import { RightSidebarProvider } from './RightSidebar';
-
-const Root: React.FC = ({ children }) => {
-  return <BrowserOnly>{() => <RightSidebarProvider>{children}</RightSidebarProvider>}</BrowserOnly>;
-};
-
-export default Root;
