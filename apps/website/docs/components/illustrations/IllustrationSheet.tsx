aca6adbc8265898710b7a2f090f753b09c65de22
Jennifer Liu <jennifer.liu@coinbase.com>
2022-03-16 07:09:38 -0700

    [trivial] docs(Illustrations): Add ability to filter illustrations through categories (#86)
    * Ported over the illustration categorization logic.
    
    * Cleaned up categorization code.
    
    * Fix lint.
    
    * Further refactor code.
    
    * More refactoring.
    
    * Fix lint.

--- apps/website/docs/components/illustrations/IllustrationSheet.tsx
+++ apps/website/docs/components/illustrations/IllustrationSheet.tsx
@@ -20,10 +20,9 @@ import {
   IllustrationSpotSquareNames,
   IllustrationVariant,
 } from '@cbhq/cds-common/types/IllustrationNames';
-import { TextInput } from '@cbhq/cds-web/controls/TextInput';
+import { SearchInput, Select, SelectOption } from '@cbhq/cds-web/controls';
 import { Illustration } from '@cbhq/cds-web/illustrations/Illustration';
-import { Box } from '@cbhq/cds-web/layout';
-import { VStack } from '@cbhq/cds-web/layout/VStack';
+import { Box, HStack, VStack } from '@cbhq/cds-web/layout';
 import { TextLabel1 } from '@cbhq/cds-web/typography/TextLabel1';
 
 const variantToNamesMap: Record<
@@ -41,17 +40,58 @@ const variantToNamesMap: Record<
   pictogram: pictogramNames,
 };
 
-const queryMatchesName = (query: string, name: string) => {
-  const queryRe = new RegExp(query.trim().toLowerCase(), 'gi');
-  const nameRe = new RegExp(name.toLowerCase(), 'gi');
+/**
+ * Determines whether the name of this illustration is
+ * in the selected category
+ * @param name - the user search term
+ * @param selectedCategory - the category chosen by the user
+ * @returns true if this illustration is a subset of the selectedCategory
+ */
+const nameInCategory = (name: string, selectedCategory: string) => {
+  if (selectedCategory === 'all') return true;
 
-  const matchedIconNames: string[] = [];
-
-  if (query in illustrationDescriptionGraph) {
-    matchedIconNames.push(...illustrationDescriptionGraph[query]);
+  // i.e if category = 'contract'
+  // then the categoryNames would be the values of contract in
+  // illustrationDescriptionGraphy
+  if (selectedCategory in illustrationDescriptionGraph) {
+    return illustrationDescriptionGraph[selectedCategory].includes(name);
   }
 
-  return name.match(queryRe) !== null || matchedIconNames.join(' ').match(nameRe) !== null;
+  return false;
+};
+
+/**
+ * Determines whether the query is a subset of the name.
+ * i.e If the name is contract, and query is con, this would return
+ * true.
+ * @param query - the user search term
+ * @param name - the name of the illustration that is being matched with
+ * @returns true if query is a subset of name of this illustration
+ */
+const queryIsSubsetOfName = (query: string, name: string) => {
+  if (query === '') return true;
+
+  const queryRe = new RegExp(query.trim().toLowerCase(), 'gi');
+
+  return queryRe.test(name);
+};
+
+/**
+ * If the query is the description of an illustration, we need
+ * to find the name of the illustration that has the description
+ * of the query.
+ * @param query - the user search term
+ * @param name - the name of the illustration that is being matched with
+ * @returns returns true if query is a description of this illustration
+ */
+const queryHasMatchingDescription = (query: string, name: string) => {
+  if (query === '') return true;
+
+  if (query in illustrationDescriptionGraph) {
+    return illustrationDescriptionGraph[query].includes(name);
+  }
+
+  return false;
 };
 
 export const IllustrationSheet = function IllustrationSheet({
@@ -60,24 +100,37 @@ export const IllustrationSheet = function IllustrationSheet({
   variant: IllustrationVariant;
 }) {
   const [query, setQuery] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const options = ['all', 'navigation illustration', 'app switcher', 'logged out'];
   const names = variantToNamesMap[variant];
   const dimensions = illustrationDimensions[variant];
   const defaultVal = illustrationDimensionDefaults[variant];
 
-  const searchOnChange = throttle((event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
+  const searchOnChange = throttle((text: string) => {
+    setQuery(text);
   }, 1000);
 
   return (
     <>
-      <Box flexWrap="wrap" spacingVertical={2}>
-        <TextInput
-          onChange={searchOnChange}
+      <HStack spacingVertical={2} gap={2}>
+        <SearchInput
+          value={query}
+          onChangeText={searchOnChange}
           type="text"
+          width="70%"
           placeholder="Illustration name"
-          label="Filter Illustrations"
         />
-      </Box>
+        <Select
+          value={selectedCategory}
+          placeholder="Choose something..."
+          width="30%"
+          onChange={setSelectedCategory}
+        >
+          {options.map((option) => (
+            <SelectOption value={option} key={option} title={option} />
+          ))}
+        </Select>
+      </HStack>
 
       <Tabs
         defaultValue={defaultVal}
@@ -89,7 +142,11 @@ export const IllustrationSheet = function IllustrationSheet({
               <Box flexWrap="wrap" spacingTop={1} spacingBottom={3}>
                 {names
                   .filter((name) => {
-                    return queryMatchesName(query, name);
+                    return (
+                      (queryHasMatchingDescription(query, name) &&
+                        nameInCategory(name, selectedCategory)) ||
+                      (queryIsSubsetOfName(query, name) && nameInCategory(name, selectedCategory))
+                    );
                   })
                   .map((filteredName) => (
                     <VStack spacing={3} alignItems="center" key={filteredName}>
