bf5345ffe5c8e23d130845af4a24960e05b70200
Katherine Martinez <katherine.martinez@coinbase.com>
2022-03-16 21:31:57 -0400

    [trivial] internal: remove yarn setup command from setup.sh (#106)
    * [trivial] internal: remove yarn setup command from setup.sh
    
    * add git fetch before setup

--- tools/ci/setup.sh
+++ tools/ci/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "--- Updating local 'master' branch"
+
+# Required for correct NX affected project resolution
+git fetch -f --no-tags origin master:master
+
 echo "--- Installing yarn dependencies"
 
 # Disable global cache so that we can cache `.yarn/cache` in buildkite
@@ -7,14 +12,5 @@ yarn config set enableGlobalCache false
 
 # Immutable is the same as a frozen lockfile
 yarn install --immutable
-
-echo "--- Updating local 'master' branch"
-
-# Required for correct NX affected project resolution
-git fetch -f --no-tags origin master:master
-
-echo "--- Building required packages"
-
-yarn setup
 
 echo "--- Setup complete, running jobs"
