022b70827ab41f7197a7d0d3e3ff72f591b5f408
Michael Chen <michael.chen@coinbase.com>
2022-03-17 16:08:24 -0700

    [trivial] fix(ModalFooter): Fix key warning (#113)
    * fix(ModalFooter): Fix key warning
    
    * fix typo

--- packages/mobile/overlays/Modal/ModalFooter.tsx
+++ packages/mobile/overlays/Modal/ModalFooter.tsx
@@ -14,7 +14,7 @@ export const ModalFooter: React.FC<ModalFooterProps> = ({
   testID,
 }) => {
   const { hideDividers = false } = useModalParent();
-  const actions = [secondaryAction, primaryAction];
+  const actions = [secondaryAction, primaryAction].filter(Boolean);
 
   // reverse actions order when stacked
   if (vertical) {
@@ -25,8 +25,10 @@ export const ModalFooter: React.FC<ModalFooterProps> = ({
     <Box spacingHorizontal={3} spacingVertical={2} testID={testID} borderedTop={!hideDividers}>
       {/* eslint-disable-next-line react-native-a11y/has-accessibility-hint */}
       <ButtonGroup accessibilityLabel="Group" block={!vertical} vertical={vertical}>
-        {actions.map((action) => (
-          <Fragment key={action?.key}>{action}</Fragment>
+        {actions.map((action, i) => (
+          // actions are stable so should be fine to use index as key
+          // eslint-disable-next-line react/no-array-index-key
+          <Fragment key={i}>{action}</Fragment>
         ))}
       </ButtonGroup>
     </Box>
