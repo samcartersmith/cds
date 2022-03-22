269fbac93b20b8460ccaedce13c7378c6c2acc6a
Katherine Martinez <katherine.martinez@coinbase.com>
2022-03-22 12:05:57 -0400

    [trivial] update(Card): add forwardRef (#143)

--- packages/mobile/alpha/Card.tsx
+++ packages/mobile/alpha/Card.tsx
@@ -1,5 +1,5 @@
-import React, { memo, useMemo } from 'react';
-import { ViewStyle } from 'react-native';
+import React, { forwardRef, memo, useMemo } from 'react';
+import { View, ViewStyle } from 'react-native';
 import type { CardBaseProps } from '@cbhq/cds-common/types/alpha';
 
 import { VStack } from '../layout/VStack';
@@ -7,51 +7,54 @@ import { OnPress, Pressable, PressableProps } from '../system/Pressable';
 import { DangerouslySetStyle } from '../types';
 
 export type CardProps = {
+  /**
+   * If onPress is present the Card will be wrapped with a Pressable component.
+   * pressableProps allows customization of that Pressable wrapper.
+   */
   pressableProps?: PressableProps;
+  children?: React.ReactNode;
 } & CardBaseProps<OnPress> &
   DangerouslySetStyle<ViewStyle>;
 
-export const Card: React.FC<CardProps> = memo(function Card({
-  children,
-  onPress,
-  dangerouslySetStyle,
-  width,
-  height,
-  testID,
-  pressableProps,
-  ...props
-}) {
-  const pressableStyles = useMemo(
-    () => ({ width, height, dangerouslySetStyle }),
-    [height, width, dangerouslySetStyle],
-  );
+export const Card = memo(
+  forwardRef<View, CardProps>(function Card(
+    { children, onPress, dangerouslySetStyle, width, height, testID, pressableProps, ...props },
+    ref,
+  ) {
+    const pressableStyles = useMemo(
+      () => ({ width, height, dangerouslySetStyle }),
+      [height, width, dangerouslySetStyle],
+    );
 
-  const content = (
-    <VStack
-      width={width}
-      height={height}
-      dangerouslySetStyle={onPress ? undefined : dangerouslySetStyle}
-      testID={onPress ? undefined : testID}
-      {...props}
-    >
-      {children}
-    </VStack>
-  );
+    const content = (
+      <VStack
+        ref={onPress ? undefined : ref}
+        width={width}
+        height={height}
+        dangerouslySetStyle={onPress ? undefined : dangerouslySetStyle}
+        testID={onPress ? undefined : testID}
+        {...props}
+      >
+        {children}
+      </VStack>
+    );
 
-  return onPress ? (
-    <Pressable
-      testID={testID}
-      backgroundColor="transparent"
-      onPress={onPress}
-      style={pressableStyles}
-      noScaleOnPress
-      {...pressableProps}
-    >
-      {content}
-    </Pressable>
-  ) : (
-    content
-  );
-});
+    return onPress ? (
+      <Pressable
+        ref={ref}
+        testID={testID}
+        backgroundColor="transparent"
+        onPress={onPress}
+        style={pressableStyles}
+        noScaleOnPress
+        {...pressableProps}
+      >
+        {content}
+      </Pressable>
+    ) : (
+      content
+    );
+  }),
+);
 
 Card.displayName = 'Card';
