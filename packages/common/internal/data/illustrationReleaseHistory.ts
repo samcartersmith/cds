3214197ea96791c4984c6630d1b4e8eced0d6006
Jennifer Liu <jennifer.liu@coinbase.com>
2022-05-13 11:34:04 -0700

    [trivial] chore(Illustration): Publish Illustrations May 12th, 2022 (#393) (#397)
    * [trivial] chore(Illustration): Publish Illustrations May 12th, 2022
    
    * Fix lint.

--- packages/common/internal/data/illustrationReleaseHistory.ts
+++ packages/common/internal/data/illustrationReleaseHistory.ts
@@ -5,4 +5,29 @@
 
 import { ReleasedIllustrationsTypes } from '../../types/IllustrationProps';
 
-export const illustrationReleaseHistory: Record<string, ReleasedIllustrationsTypes> = {};
+export const illustrationReleaseHistory: Record<string, ReleasedIllustrationsTypes> = {
+  'Thu May 12 2022': {
+    newIllustrations: [
+      'heroSquare/brdGift-dark',
+      'heroSquare/brdGift-light',
+      'heroSquare/hiddenCollection-dark',
+      'heroSquare/hiddenCollection-light',
+      'heroSquare/receiveGift-dark',
+      'heroSquare/receiveGift-light',
+      'pictogram/advancedTradingNavigation-dark',
+      'pictogram/advancedTradingNavigation-light',
+      'pictogram/directDepositNavigation-dark',
+      'pictogram/directDepositNavigation-light',
+      'pictogram/helpCenterNavigation-dark',
+      'pictogram/helpCenterNavigation-light',
+      'pictogram/taxCenterNavigation-dark',
+      'pictogram/taxCenterNavigation-light',
+      'spotRectangle/login-dark',
+      'spotRectangle/login-light',
+      'spotSquare/miniGift-dark',
+      'spotSquare/miniGift-light',
+    ],
+    modifiedIllustrations: [],
+    deletedIllustrations: [],
+  },
+};
