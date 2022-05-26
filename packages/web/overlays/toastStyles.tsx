bc8f3dde32fc83b3873cd5bd759b8be8a6c78f01
Michael Chen <michael.chen@coinbase.com>
2022-05-26 16:04:54 -0700

    [trivial] update(Toast): Migrate Toast animation to Framer (#494)
    * migrate toast
    
    * remove initial
    
    * use portal component
    
    * Empty-Commit
    
    * update tests

--- packages/web/overlays/toastStyles.tsx
+++ packages/web/overlays/toastStyles.tsx
@@ -1,25 +1,16 @@
 import { css } from 'linaria';
-import { animateInBottomConfig, animateInOpacityConfig } from '@cbhq/cds-common/animation/toast';
 
 import { spacing } from '../tokens';
 import { cx } from '../utils/linaria';
 
 export const toastStaticClassName = 'cds-toast';
 
-// animated property
-const toastAnimationStyles = {
-  [animateInOpacityConfig.property]: animateInOpacityConfig.fromValue as number,
-};
-
 export const toastPosition = css`
   &.${toastStaticClassName} {
-    ${toastAnimationStyles}
     position: fixed;
     bottom: ${spacing[4]};
     left: 0;
     width: 100%;
-    justify-content: center;
-    transform: translateY(${animateInBottomConfig.fromValue as number}px);
   }
 `;
 
