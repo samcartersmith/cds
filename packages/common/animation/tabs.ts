9c9f3f3c34f8d1797455b5325c3dcc4b43004260
Michael Chen <michael.chen@coinbase.com>
2022-05-31 10:34:20 -0700

    [trivial] update(TabIndicator): Migrate TabIndicator animation to Framer (#496)
    * migrate tabindicator
    
    * fix import
    
    * Empty-Commit

--- packages/common/animation/tabs.ts
+++ packages/common/animation/tabs.ts
@@ -1,8 +1,9 @@
 import { MotionBaseSpec } from '../types';
 
-type TabIndicatorMotionBaseSpec = Pick<MotionBaseSpec, 'easing' | 'duration'>;
+type TabIndicatorMotionBaseSpec = Pick<MotionBaseSpec, 'easing' | 'duration' | 'property'>;
 
 export const animateTabIndicatorBaseSpec: TabIndicatorMotionBaseSpec = {
+  property: 'x',
   easing: 'global',
   duration: 'moderate1',
 };
