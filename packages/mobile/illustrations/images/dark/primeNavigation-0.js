5c756d93cc9062f5729b7d7b8ac1029227f8e76c
Jennifer Liu <jennifer.liu@coinbase.com>
2022-06-15 09:10:18 -0700

    [trivial] chore(illustrations): Publish 06/08/22 [mm/dd/yy] Illo (#638)
    * [trivial] chore(illustrations): Publish 06/09/22 [mm/dd/yy] Illo
    
    * [trivial] chore(illustrations): Publish 06/08/22 [mm/dd/yy] Illo

--- packages/mobile/illustrations/images/dark/primeNavigation-0.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  content: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 48 48"><path fill="#fff" d="M44 24H24l14.125 14.125C41.75 34.5 44 29.5 44 24z"/><path fill="#588AF5" d="M24 24V4C12.937 4 4 12.938 4 24c0 11.063 8.938 20 20 20 5.5 0 10.5-2.25 14.125-5.875L24 24z"/><path fill="#464B55" d="M34 6.688 24 24h20c0-7.375-4-13.875-10-17.313z"/></svg>`,
-};
