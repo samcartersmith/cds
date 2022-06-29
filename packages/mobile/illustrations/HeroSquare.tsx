b2072b31dc5f1381f98e068dbe7123d5acf14cdc
Katherine Martinez <katherine.martinez@coinbase.com>
2022-06-29 14:05:14 -0400

    [trivial] internal(website): fix website deploys by point changelog path to correct file (#800)

--- packages/mobile/illustrations/HeroSquare.tsx
+++ packages/mobile/illustrations/HeroSquare.tsx
@@ -3,6 +3,6 @@ import { createIllustration } from './createIllustration';
 export type { HeroSquareProps } from '@cbhq/cds-common/types/IllustrationProps';
 
 /**
- * @changelog ./illustrationSpectrumMap.ts, ./createIllustration.tsx, ./Illustration.tsx, .useIllustrationXml.ts
+ * @changelog ./illustrationSpectrumMap.ts, ./createIllustration.tsx, ./Illustration.tsx, ./useIllustrationXml.ts
  */
 export const HeroSquare = createIllustration('heroSquare');
