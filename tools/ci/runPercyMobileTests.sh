57c85559ead6717934932608c8d14aae9a1d8c82
David Maulick <david.maulick@coinbase.com>
2022-07-26 10:54:16 -0700

    [trivial] internal(Mobile Visual Regression Tests): Disable Android vis reg until resolve flake (#877)
    * disable
    
    * comment

--- tools/ci/runPercyMobileTests.sh
+++ tools/ci/runPercyMobileTests.sh
@@ -8,4 +8,5 @@ echo "$randomNonce" > "percy-artifacts/$PERCY_NONCE_FILE_NAME"
 
 # If we do not exit and nonce is succesfully created; we run iOS and Android tests
 buildkite-agent pipeline upload .buildkite/percy-ios.yml
-buildkite-agent pipeline upload .buildkite/percy-android.yml
+# Add back when not flaky:
+# buildkite-agent pipeline upload .buildkite/percy-android.yml
