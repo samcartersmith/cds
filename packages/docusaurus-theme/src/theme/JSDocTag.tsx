e47cb9e718c36fdc4b65838a96684bb17425265b
Jake LeBoeuf <jake.leboeuf@coinbase.com>
2022-09-01 15:59:24 -0400

    [CDS-2437] internal(Docs): Use CDS tag for our docs (#1051)

--- packages/docusaurus-theme/src/theme/JSDocTag.tsx
+++ packages/docusaurus-theme/src/theme/JSDocTag.tsx
@@ -1,8 +1,6 @@
 import React, { memo } from 'react';
 import type { JSDocTagProps, JSDocTagVariant } from '@theme/JSDocTag';
-import { Box } from '@cbhq/cds-web/layout';
-import { ThemeProvider } from '@cbhq/cds-web/system';
-import { TextLabel1 } from '@cbhq/cds-web/typography';
+import { Tag } from '@cbhq/cds-web/tag/Tag';
 
 export const JSDOC_TAG_VARIANTS = [
   'beta',
@@ -13,26 +11,31 @@ export const JSDOC_TAG_VARIANTS = [
   'required',
 ] as const;
 
-const JSDOC_TAG_HEIGHT = 24;
-
-const JSDOC_TAG_STYLES = {
-  padding: '2px 6px 2px 6px',
-  borderRadius: JSDOC_TAG_HEIGHT,
-};
-
-const COLOR_PRESETS = {
-  blue: { primary: 'blue0', primaryForeground: 'blue60' },
-  green: { primary: 'green0', primaryForeground: 'green60' },
-  red: { primary: 'red0', primaryForeground: 'red60' },
-} as const;
-
-const BADGE_PALETTES = {
-  beta: COLOR_PRESETS.green,
-  danger: COLOR_PRESETS.red,
-  deprecated: COLOR_PRESETS.red,
-  internal: COLOR_PRESETS.red,
-  new: COLOR_PRESETS.blue,
-  required: COLOR_PRESETS.blue,
+const TAG_PROPS = {
+  beta: {
+    colorScheme: 'yellow',
+    intent: 'promotional',
+  },
+  danger: {
+    colorScheme: 'red',
+    intent: 'promotional',
+  },
+  deprecated: {
+    colorScheme: 'red',
+    intent: 'informational',
+  },
+  internal: {
+    colorScheme: 'blue',
+    intent: 'informational',
+  },
+  new: {
+    colorScheme: 'blue',
+    intent: 'promotional',
+  },
+  required: {
+    colorScheme: 'gray',
+    intent: 'informational',
+  },
 } as const;
 
 const LABELS: Record<JSDocTagVariant, string> = {
@@ -44,25 +47,8 @@ const LABELS: Record<JSDocTagVariant, string> = {
   required: 'Required',
 };
 
-/**
- * @todo Replace with official CDS Tag components when those are ready
- *
- */
 const JSDocTag = memo(function JSDocTag({ variant }: JSDocTagProps) {
-  return (
-    <ThemeProvider display="contents" palette={BADGE_PALETTES[variant]}>
-      <Box
-        width="fit-content"
-        height={JSDOC_TAG_HEIGHT}
-        background="primary"
-        dangerouslySetStyle={JSDOC_TAG_STYLES}
-      >
-        <TextLabel1 as="span" transform="none" color="primaryForeground">
-          {LABELS[variant] ?? variant}
-        </TextLabel1>
-      </Box>
-    </ThemeProvider>
-  );
+  return <Tag {...TAG_PROPS[variant]}>{LABELS[variant] ?? variant}</Tag>;
 });
 
 export default JSDocTag;
