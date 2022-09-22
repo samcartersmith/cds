44f7db6be9475dff27b1ce4c593a67ca840bb527
Katherine Martinez <katherine.martinez@coinbase.com>
2022-09-22 12:24:47 -0400

    [trivial] fix: ignore assets folder created on build (#1143)

--- packages/ui-mobile-a11y-engine/src/assets/images-png.d.ts
+++ packages/ui-mobile-a11y-engine/src/assets/images-png.d.ts
@@ -1,4 +1,4 @@
 declare module '*.png' {
-  const value: string;
+  const value: import('react-native').ImageSourcePropType;
   export default value;
 }
--- packages/ui-mobile-a11y-engine/src/assets/index.ts
+++ packages/ui-mobile-a11y-engine/src/assets/index.ts
@@ -1,5 +1,3 @@
-import { ImageSourcePropType } from 'react-native';
-
 import filledHeart16 from './filled_heart_16px.png';
 import filledHeart24 from './filled_heart_24px.png';
 import filledHeart32 from './filled_heart_32px.png';
@@ -7,11 +5,11 @@ import heart32 from './heart_32px.png';
 
 export const TestAssets = {
   heart: {
-    '32px': heart32 as ImageSourcePropType,
+    '32px': heart32,
   },
   filledHeart: {
-    '16px': filledHeart16 as ImageSourcePropType,
-    '24px': filledHeart24 as ImageSourcePropType,
-    '32px': filledHeart32 as ImageSourcePropType,
+    '16px': filledHeart16,
+    '24px': filledHeart24,
+    '32px': filledHeart32,
   },
 };
