6a9646638af4c9ab900e5c398d0fe1636d8408ad
Jennifer Liu <jennifer.liu@coinbase.com>
2022-10-04 10:53:01 -0700

    [trivial] internal(Illo Script): Simplified illustration script. Put uploadToStaticAssets step in codegen illustration release (#1156)
    * [trivial] internal(Illustration Script): Create an nx command to upload files to staticAssets
    
    * Format file to fit shell conventions. Add date to commit message.
    
    * Adding a shell formatter, so we are on the same page for formatting.

--- docs/release/illustrations.md
+++ docs/release/illustrations.md
@@ -1,6 +1,6 @@
 ## Release Illustrations
 
-Prior to running release make sure you have done the following:
+Prior to running release make sure you have done the following. This should be a 1 time setup. You shouldn't need this afterwards:
 
 1. install hub using homebrew (a CLI tool for managing github repo)
 2. https://confluence.coinbase-corp.com/display/INFRA/How+to+Fork+a+Repository - Follow these instruction to make sure that you have all the config to make a fork from Coinbase Repo
@@ -8,19 +8,16 @@ Prior to running release make sure you have done the following:
 
 Now lets release the illustrations:
 
-1. Checkout a branch called `<lastName>/illos-mm-dd-yyyy` with pr title `[trivial] chore(Illustrations): Publish illustrations mm/ddyyyy`. [Exmaple for titling](https://github.cbhq.net/frontend/cds/pull/1108)
+1. Checkout a branch called `<lastName>/illos-mm-dd-yyyy` with pr title `[trivial] chore(Illustrations): Publish illustrations mm/dd/yyyy`. [Example for titling](https://github.cbhq.net/frontend/cds/pull/1108)
 
-2. Run `yarn nx run codegen:illustrations`
-3. Verify that the generated illustrations match what is in the illustrations section at [go/icon-illo-release-history](https://docs.google.com/document/d/19N5EpYixyttCbmi2pbO0DsC0waL14v77MrwR7OQrwJQ/edit#heading=h.v54y1yynd69b)
-4. Next you need to deploy the assets. Run `cd packages/codegen/illustrations` because the script expects this to be the cwd (current working directory).
-5. Run `./uploadToStaticAssets.sh` to upload the images
-
-Note: After running the upload command, all .png files should be deleted. These leftover files are what you will commit to CDS in later steps.
-
-6. Scroll up in the output of the script and find the link to the geneated pull request link against https://github.cbhq.net/engineering/static-assets.
-
-7. Follow the link and land the pr against https://github.cbhq.net/engineering/static-assets. When it lands the assets will be uploaded to an s3 bucket that will serve them.
+2. Run `yarn nx run codegen:illustrations`. This command will generate another PR to upload illustrations to static-assets. You need to merge that PR in first, so that
+   it uploads those assets onto CDN. Then, it will render well on Percy.
+3. Scroll up in the output of the script and find the link to the geneated pull request link against https://github.cbhq.net/engineering/static-assets.
+4. Follow the link and land the pr against https://github.cbhq.net/engineering/static-assets. When it lands the assets will be uploaded to an s3 bucket that will serve them.
    (Example pr: https://github.cbhq.net/engineering/static-assets/pull/774)
+5. Verify that the Percy changes for illustrations matches [go/icon-illo-release-history](https://docs.google.com/document/d/19N5EpYixyttCbmi2pbO0DsC0waL14v77MrwR7OQrwJQ/edit#heading=h.v54y1yynd69b)
+
+Note: After running the upload command, all .png files should be deleted. PNG files should not be commited at all.
 
 It takes about 15 min for the changes landed against https://github.cbhq.net/engineering/static-assets to take affect. If you do not wait percy will be incorrect on our CI because the images will not be available yet.
 
--- packages/codegen/illustrations/uploadToStaticAssets.sh
+++ packages/codegen/illustrations/uploadToStaticAssets.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# A script to programmatically upload illustrations  
+# A script to programmatically upload illustrations
 # to static-asset repo
 
 ## PREREQUISITES ##
-# Before you can run this script, you must have the following 
-# items completed:  
-# 1. install hub using homebrew (a CLI tool for managing github repo) 
-# 2. https://confluence.coinbase-corp.com/display/INFRA/How+to+Fork+a+Repository - Follow these instruction 
-#    to make sure that you have all the config to make a fork from Coinbase Repo 
+# Before you can run this script, you must have the following
+# items completed:
+# 1. install hub using homebrew (a CLI tool for managing github repo)
+# 2. https://confluence.coinbase-corp.com/display/INFRA/How+to+Fork+a+Repository - Follow these instruction
+#    to make sure that you have all the config to make a fork from Coinbase Repo
 # 3. install pngcrush using homebrew (a CLI tool to compress pngs)
 
 ## Troubleshoot Guide ##
 
-# If you are having issue with permission denied when running this script. 
-# run chmod u+x uploadToStaticAssets.sh (or path/to/script). This will give 
-# it access to the script. 
+# If you are having issue with permission denied when running this script.
+# run chmod u+x uploadToStaticAssets.sh (or path/to/script). This will give
+# it access to the script.
 
 # USAGE: ./uploadToStaticAssets.sh [optional flags: -r -b -u -c -o -d]
 
-# The most basic usage without any optional flags. You will probably run it like this 
-# 90% of the time 
+# The most basic usage without any optional flags. You will probably run it like this
+# 90% of the time
 # Basic Example: ./uploadToStatic.sh
 
 # Example on how to set root path. In case we detected the wrong root path
-# Setting rootpath Example: ./uploadToStatic.sh -r /path/to/monorepo 
+# Setting rootpath Example: ./uploadToStatic.sh -r /path/to/monorepo
 
 # Example on how to set custom branch name
 # Setting Branchname Example: ./uploadToStatic.sh -r /path/to/monorepo -b publish-illo-today
 
 # Example on how to set custom username. In case, this script is unable to detect your username
-# With Username Example: ./uploadToStatic.sh -r /path/to/monorepo -u jennifer-liu 
+# With Username Example: ./uploadToStatic.sh -r /path/to/monorepo -u jennifer-liu
 
 # -u (USERNAME) = set your github username
 # -b (BRANCHNAME) = set the branch name of this cloned static asset
@@ -39,13 +39,13 @@
 # -d (DELREPO) = if true, will remove static-assets repo. default: true
 
 usage() {
-  echo "USAGE: ./uploadToStaticAssets -r <root-dir-name> [optional flags: -b (BRANCHNAME) -u (USERNAME) -c (CREATEDIRFLAG) -o (OPENPRFLAG) -d (DELREPO)]"
+	echo "USAGE: ./uploadToStaticAssets -r <root-dir-name> [optional flags: -b (BRANCHNAME) -u (USERNAME) -c (CREATEDIRFLAG) -o (OPENPRFLAG) -d (DELREPO)]"
 }
 
 getGithubUsername() {
-	email=`git config user.email`
+	email=$(git config user.email)
 
-	IFS='.' # setting period as delimiter
+	IFS='.'                    # setting period as delimiter
 	read -ra EMAIL <<<"$email" #reading email as an array as tokens separated by IFS
 
 	firstName=${EMAIL[0]}
@@ -59,16 +59,15 @@ getGithubUsername() {
 }
 
 getRootPath() {
-	root=`npm root`
-	IFS='/' #setting / as delimiter
+	root=$(npm root)
+	IFS='/'                  #setting / as delimiter
 	read -ra ROOT <<<"$root" #reading root as an array as tokens separated by IFS
 
 	ROOT_LEN=${#ROOT[@]}
 	SLASH="/"
 
 	rootPath=""
-	for (( i=1; i<$(expr $ROOT_LEN - 1); i++ ))
-	do
+	for ((i = 1; i < $(expr $ROOT_LEN - 1); i++)); do
 		rootPath="$rootPath${SLASH}${ROOT[i]}"
 	done
 	echo "$rootPath/packages/codegen/illustrations"
@@ -77,9 +76,9 @@ getRootPath() {
 compressPNGs() {
 	# Obtain the size of png directory
 	# before it is compressed
-	uncompressedSize=`du $1`
+	uncompressedSize=$(du $1)
 
-	cd $1 
+	cd $1
 
 	for dir in *.png; do
 		# Obtain filename
@@ -99,50 +98,46 @@ compressPNGs() {
 
 	# Obtain the size of png directory
 	# after it is compressed
-	compressedSize=`du $1`
+	compressedSize=$(du $1)
 
-	echo "Path: $1"	
+	echo "Path: $1"
 	echo "Uncompressed Size: $uncompressedSize"
 	echo "Compressed Size: $compressedSize"
-} 
+}
 
-while getopts b:r:u:c:o:d: option
-do
+while getopts b:r:u:c:o:d: option; do
 	case "${option}" in
-		b) BRANCHNAME=${OPTARG};;
-		r) ROOTDIRPATH=${OPTARG};;
-		u) USERNAME=${OPTARG};;
-		c) CREATEDIRFLAG=${OPTARG};;
-		o) OPENPRFLAG=${OPTARG};;
-		d) DELREPO=${OPTARG};;
+	b) BRANCHNAME=${OPTARG} ;;
+	r) ROOTDIRPATH=${OPTARG} ;;
+	u) USERNAME=${OPTARG} ;;
+	c) CREATEDIRFLAG=${OPTARG} ;;
+	o) OPENPRFLAG=${OPTARG} ;;
+	d) DELREPO=${OPTARG} ;;
 	esac
 done
 
 # define username if its not set by user
-if [ -z "${USERNAME}" ]
-then
-	USERNAME=`getGithubUsername`
+if [ -z "${USERNAME}" ]; then
+	USERNAME=$(getGithubUsername)
 fi
 
-# define branchname if its not set by users 
+# define branchname if its not set by users
 # The default branch name will be publish-illo-<today's name>-<random number>
-if [ -z "${BRANCHNAME}" ]
-then
+if [ -z "${BRANCHNAME}" ]; then
 	now=$(date +'%m-%d-%Y')
 	BRANCHNAME="publish-illo-${now}-$RANDOM"
 fi
 
-# define rootPath if its not set by users 
-if [ -z "${ROOTDIRPATH}" ]
-then
-	ROOTDIRPATH=`getRootPath`
+# define rootPath if its not set by users
+if [ -z "${ROOTDIRPATH}" ]; then
+	ROOTDIRPATH=$(getRootPath)
 fi
 
 echo "FLAGS --- BRANCHNAME: ${BRANCHNAME}, ROOTDIRPATH: ${ROOTDIRPATH}, USERNAME: ${USERNAME}, CREATEDIRFLAG: ${CREATEDIRFLAG}, OPENPRFLAG: ${OPENPRFLAG}, DELREPO: ${DELREPO}."
 
 # Optimize PNGs so they are smallest size possible
-pngLight=`compressPNGs "${ROOTDIRPATH}/images/png-light/"`
-pngDark=`compressPNGs "${ROOTDIRPATH}/images/png-dark/"`
+pngLight=$(compressPNGs "${ROOTDIRPATH}/images/png-light/")
+pngDark=$(compressPNGs "${ROOTDIRPATH}/images/png-dark/")
 echo $pngLight
 echo $pngDark
 
@@ -151,10 +146,9 @@ gcf2 engineering/static-assets $BRANCHNAME
 
 echo "Copying images from illustrations component to static/assets/design-system/illustrations folder"
 cd static-assets
-git pull origin "${USERNAME}/${BRANCHNAME}" 
+git pull origin "${USERNAME}/${BRANCHNAME}"
 
-if [ ${CREATEDIRFLAG=false} == true ] 
-then
+if [ ${CREATEDIRFLAG=false} == true ]; then
 	echo "Creating design-system/illustration directory..."
 	mkdir -p design-system/illustrations
 fi
@@ -163,9 +157,8 @@ echo "Copying images to static-assets..."
 cp -rf "${ROOTDIRPATH}/images/" "${ROOTDIRPATH}/static-assets/assets/design-system/illustrations/"
 
 echo "Pushing changes to static-assets"
-git add -A &&  git commit -m "Publish Illustration $(date +'%m-%d-%Y')" && git push --set-upstream origin "${USERNAME}/${BRANCHNAME}" 
-if [ ${OPENPRFLAG=false} == true ]
-then
+git add -A && git commit -m "Publish Illustration $(date +'%m-%d-%Y')" && git push --set-upstream origin "${USERNAME}/${BRANCHNAME}"
+if [ ${OPENPRFLAG=false} == true ]; then
 	echo "Creating PR..."
 	hub pull-request
 fi
@@ -174,8 +167,7 @@ fi
 # Deletes the cloned static-assets repo
 # Deletes the png-dark and png-light directory so we
 # don't commit so much to git
-if [ ${DELREPO=true} == true ]
-then
+if [ ${DELREPO=true} == true ]; then
 	echo "Removing static-assets directory"
 	cd .. && rm -rf static-assets
 	echo "Deleting png-dark directory"
