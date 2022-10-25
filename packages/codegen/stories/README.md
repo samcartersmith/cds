0aac1de2f4a80fa37ef7ae4cee4f4b175ac47e3c
Jennifer Liu <jennifer.liu@coinbase.com>
2022-10-25 11:31:20 -0700

    [CDS-2474] internal(Storybook Upgrade): Codegenerate Illustration stories. Migrate away from using storiesOf (#1262)
    * Create scripts to add illustrations.
    
    * [trivial] internal(Storybook Upgrade): Migrate away from storiesOf to support Storybook v7.
    
    * Fix story format.
    
    * Fix Illustration stories name to match old one.
    
    * Increase chunk size.
    
    * Add illustration generation to project.json.
    
    * Update illustration sheet.
    
    * Update project.json for codegen.
    
    * Remove static-assets.
    
    * Repush.
    
    * Move constant variable to global scope.

--- packages/codegen/stories/README.md
+++ packages/codegen/stories/README.md
@@ -1,6 +1,6 @@
 # Stories folder
 
-The stories folder stores functions that can codegenerates stories that have dynamic data.
+The stories folder stores functions that codegenerates stories that have dynamic data.
 
 This is necessary because Storybook v7 onwards is no longer supporting storiesOf feature. Hence, stories like Icons and Illustrations can no longer use this feature to chunk the stories up.
 
--- packages/web/illustrations/__stories__/Illustration.stories.tsx
+++ packages/web/illustrations/__stories__/Illustration.stories.tsx
@@ -1,11 +1,19 @@
-import { storiesOf } from '@storybook/react';
-import { sortedImg } from '@cbhq/cds-common/internal/data/sortedIllustrationData';
+/**
+ * DO NOT MODIFY
+ * Generated from packages/codegen/stories/createIllustrationStories.ts
+ */
+
 import { illustrationPercyBuilder } from '@cbhq/cds-common/internal/illustrationPercyBuilder';
 
 import { Box, HStack, VStack } from '../../layout';
 import { ThemeProvider } from '../../system';
 import { TextLabel1 } from '../../typography';
 import { Illustration } from '../Illustration';
+
+export default {
+  title: 'Illustrations',
+  component: Illustration,
+};
 
 const { ListIllustrations } = illustrationPercyBuilder(
   Illustration,
@@ -16,14 +24,15 @@ const { ListIllustrations } = illustrationPercyBuilder(
   (props) => <TextLabel1 {...props} as="p" />,
 );
 
-let stories = storiesOf('Illustrations', module);
-
-const numIllustrations = sortedImg.length;
-
-let i = 0;
-const CHUNK_SIZE = 120;
-
-for (let j = 0; j < numIllustrations; j += CHUNK_SIZE) {
-  stories = stories.add(`Sheet ${i}`, () => ListIllustrations(j, j + CHUNK_SIZE));
-  i += 1;
-}
+export const Sheet0 = () => ListIllustrations(0, 120);
+export const Sheet1 = () => ListIllustrations(120, 240);
+export const Sheet2 = () => ListIllustrations(240, 360);
+export const Sheet3 = () => ListIllustrations(360, 480);
+export const Sheet4 = () => ListIllustrations(480, 600);
+export const Sheet5 = () => ListIllustrations(600, 720);
+export const Sheet6 = () => ListIllustrations(720, 840);
+export const Sheet7 = () => ListIllustrations(840, 960);
+export const Sheet8 = () => ListIllustrations(960, 1080);
+export const Sheet9 = () => ListIllustrations(1080, 1200);
+export const Sheet10 = () => ListIllustrations(1200, 1320);
+export const Sheet11 = () => ListIllustrations(1320, 1440);
