519aad66b404ed1772e9d76b05c2ec736a642a57
Blair McKee <blair.mckee@coinbase.com>
2022-11-01 12:05:20 -0500

    [trivial] tests: migrated textinput interactive test to new format (#1308)
    * [trivial] tests: migrated textinput interactive test to new format
    
    * [trivial] chore: changed taxonomy

--- packages/web/controls/__stories__/TextInputInteractive.stories.tsx
+++ packages/web/controls/__stories__/TextInputInteractive.stories.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import { expect } from '@storybook/jest';
-import { ComponentStory } from '@storybook/react';
+import { ComponentStoryObj } from '@storybook/react';
 import { userEvent, within } from '@storybook/testing-library';
 
 import { AddressForm } from './AddressForm';
 
 export default {
-  title: 'Interactive/Inputs',
+  title: 'Interactive/TextInput',
   component: AddressForm,
 };
 
-const Template: ComponentStory<typeof AddressForm> = (args) => <AddressForm {...args} />;
-
-export const Filled = Template.bind({});
-
-Filled.args = {
-  onSubmit: (e: React.FormEvent) => {
-    e.preventDefault();
+export const FormFilled: ComponentStoryObj<typeof AddressForm> = {
+  args: {
+    onSubmit: (e: React.FormEvent) => {
+      e.preventDefault();
+    },
   },
-};
-
-Filled.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  userEvent.type(canvas.getByPlaceholderText('4321 Jade Palace'), '221 Stockholm Street');
-  userEvent.type(canvas.getByLabelText('City/town'), 'San Jose');
-  userEvent.type(canvas.getByLabelText('State'), 'California');
-  userEvent.type(canvas.getByLabelText('Postal code'), '89201');
-  userEvent.type(canvas.getByLabelText('Country'), 'United States');
-  userEvent.click(canvas.getByTestId('save-btn'));
-  expect(canvas.getByText('Submit button was clicked')).toBeInTheDocument();
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    userEvent.type(canvas.getByPlaceholderText('4321 Jade Palace'), '221 Stockholm Street');
+    userEvent.type(canvas.getByLabelText('City/town'), 'San Jose');
+    userEvent.type(canvas.getByLabelText('State'), 'California');
+    userEvent.type(canvas.getByLabelText('Postal code'), '89201');
+    userEvent.type(canvas.getByLabelText('Country'), 'United States');
+    userEvent.click(canvas.getByTestId('save-btn'));
+    expect(canvas.getByText('Submit button was clicked')).toBeInTheDocument();
+  },
 };
