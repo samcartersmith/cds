7ca03ce90bd15e91a3d83f530b29f7b5597c5cf0
Katherine Martinez <katherine.martinez@coinbase.com>
2023-02-02 13:19:24 -0500

    [trivial] internal: run first icon sync for @cbhq/cds-icons (#1640)
    * [trivial] internal: run first icon sync
    
    * undo reformatting to project.jsons
    
    * rebase master and re-run sync

---
+++ packages/icons/src/__generated__/nav/data/names.ts
@@ -0,0 +1,77 @@
+/**
+ * DO NOT MODIFY
+ * Generated from yarn nx run icons:sync
+ */
+
+import type { NavIconName } from '../types/NavIconName';
+
+/**
+ * An array of all NavIcon icons.
+ * This is being used to display a sheet of all NavIcon illustration on the CDS website.
+ */
+const names: NavIconName[] = [
+  'account',
+  'activity',
+  'addPeople',
+  'appSwitcher',
+  'application',
+  'astronautHelmet',
+  'bell',
+  'browser',
+  'caret',
+  'cash',
+  'chartBar',
+  'chartCandles',
+  'chartPie',
+  'cloud',
+  'collection',
+  'comment',
+  'copy',
+  'crystalBall',
+  'dashboard',
+  'defi',
+  'diamond',
+  'directDeposit',
+  'documentation',
+  'drops',
+  'earn',
+  'eye',
+  'faces',
+  'gab',
+  'ghost',
+  'giftBox',
+  'hammer',
+  'heart',
+  'hiddenEye',
+  'home',
+  'interest',
+  'invoice',
+  'light',
+  'magnifyingGlass',
+  'menu',
+  'moon',
+  'moreVertical',
+  'newsfeed',
+  'participate',
+  'pay',
+  'paymentCard',
+  'pencil',
+  'peopleGroup',
+  'planet',
+  'queryTransact',
+  'rocketShip',
+  'royalty',
+  'scanQrCode',
+  'securityShield',
+  'settings',
+  'shoppingCart',
+  'sun',
+  'support',
+  'tag',
+  'taxes',
+  'trading',
+  'trophyCup',
+  'wallet',
+];
+
+export default names;
