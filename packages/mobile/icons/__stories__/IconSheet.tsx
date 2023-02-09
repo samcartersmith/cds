64127a8a98afe81066f3fbe6e8bb6f5795397a27
Katherine Martinez <katherine.martinez@coinbase.com>
2023-02-09 16:20:26 -0500

    [trivial] internal: cleanup old icons codegen (#1670)
    * [trivial] internal: cleanup old icons codegen
    
    * delete only names from iconData

--- packages/mobile/icons/__stories__/IconSheet.tsx
+++ packages/mobile/icons/__stories__/IconSheet.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { IconName, NavigationIconName } from '@cbhq/cds-common';
+import { IconName } from '@cbhq/cds-common';
 import { ScaleProvider } from '@cbhq/cds-common/scale/ScaleProvider';
+import names from '@cbhq/cds-icons/__generated__/ui/data/names';
 
 import { HStack, VStack } from '../../layout';
 import { Icon } from '../Icon';
-import { NavigationIcon } from '../NavigationIcon';
-
-// import names from '@cbhq/cds-icons/__generated__/ui/data/names';
-import { oldIconSheetNames, TemporarySheetName } from './oldIconSheetNames';
-
-function isIcon(name: TemporarySheetName): name is IconName {
-  return typeof name === 'string';
-}
 
 function IconSheetRow({ name }: { name: IconName }) {
   return (
@@ -28,47 +21,12 @@ function IconSheetRow({ name }: { name: IconName }) {
   );
 }
 
-function NavigationIconSheetRow({ name, active }: { name: NavigationIconName; active?: boolean }) {
-  return (
-    <VStack>
-      <HStack spacingBottom={2} gap={2}>
-        <ScaleProvider value="xSmall">
-          <HStack gap={2}>
-            <NavigationIcon name={name} size="s" active={active} />
-            <NavigationIcon name={name} size="m" active={active} />
-          </HStack>
-        </ScaleProvider>
-        {(['s', 'm', 'l'] as const).map((size) => {
-          return (
-            <NavigationIcon
-              key={`nav-icon-${name}-${size}`}
-              name={name}
-              size={size}
-              active={active}
-            />
-          );
-        })}
-      </HStack>
-    </VStack>
-  );
-}
-
 export function IconSheet() {
   return (
     <VStack>
       <HStack gap={2} flexWrap="wrap">
-        {oldIconSheetNames.map((item) => {
-          if (isIcon(item)) {
-            return <IconSheetRow key={`icon-row-${item}`} name={item} />;
-          }
-          const [name, props] = item;
-          return (
-            <NavigationIconSheetRow
-              key={`nav-icon-${name}-${!!props.active}`}
-              name={name}
-              {...props}
-            />
-          );
+        {names.map((item) => {
+          return <IconSheetRow key={`icon-row-${item}`} name={item} />;
         })}
       </HStack>
     </VStack>
--- packages/web/icons/__stories__/Icon.stories.tsx
+++ packages/web/icons/__stories__/Icon.stories.tsx
@@ -1,8 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
 import { Icon } from '../Icon';
 
-import { IconSheet } from './deprecatedIconSheetBuilder';
 import { IconSheet as NewIconSheet } from './IconSheet';
 
 export default {
@@ -11,17 +8,3 @@ export default {
 };
 
 export const All = () => <NewIconSheet />;
-
-/* -------------------------------------------------------------------------- */
-/*           Everything below should be deleted after icon migration          */
-/* -------------------------------------------------------------------------- */
-
-export const ui_0 = () => IconSheet('ui', 0, 50);
-export const ui_1 = () => IconSheet('ui', 50, 100);
-export const ui_2 = () => IconSheet('ui', 100, 150);
-export const ui_3 = () => IconSheet('ui', 150, 200);
-export const ui_4 = () => IconSheet('ui', 200, 250);
-export const ui_5 = () => IconSheet('ui', 250, 300);
-export const nav_0 = () => IconSheet('nav', 0, 50);
-export const nav_1 = () => IconSheet('nav', 50, 100);
-export const nav_2 = () => IconSheet('nav', 100, 150);
