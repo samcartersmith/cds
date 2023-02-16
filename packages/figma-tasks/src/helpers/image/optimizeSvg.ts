543253238f0a1573874afa311b2897a7fc358307
Katherine Martinez <katherine.martinez@coinbase.com>
2023-02-16 00:54:40 -0500

    [trivial] fix(figma-tasks): I accidentally commited this change (#1707)

--- packages/figma-tasks/src/helpers/image/optimizeSvg.ts
+++ packages/figma-tasks/src/helpers/image/optimizeSvg.ts
@@ -4,5 +4,5 @@ import svgoConfig from './svgoConfig';
 
 export function optimizeSvg(content: string): string {
   const optimizedSvg = optimize(content, svgoConfig);
-  return optimizedSvg.data.replaceAll('#0A0B0D', '#1652F0');
+  return optimizedSvg.data;
 }
