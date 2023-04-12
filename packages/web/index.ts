7b7dac1987cae968ce57860945fe95ece522d973
David Maulick <david.maulick@coinbase.com>
2023-04-12 10:01:55 -0600

    clean up web-overlays migration (#1908)

--- packages/web/index.ts
+++ packages/web/index.ts
@@ -1,9 +1,3 @@
 // Only export types for downstream consumers
-/**
- * @deprecated this component will be removed from cds-web Q22023. It has been moved to cds-web-overlays.
- */
 export * from './types';
-/**
- * @deprecated this component will be removed from cds-web Q22023. It has been moved to cds-web-overlays.
- */
 export * from '@cbhq/cds-common';
--- tools/migrations/cds-web-overlays/applyAllPatches.sh
+++
@@ -1,5 +0,0 @@
-git apply tools/migrations/cds-web-overlays/compound-stories.patch
-git apply tools/migrations/cds-web-overlays/dropdown.patch
-git apply tools/migrations/cds-web-overlays/navigation.patch
-git apply tools/migrations/cds-web-overlays/overlays.patch
-git apply tools/migrations/cds-web-overlays/popover-select-sidebar-tooltip.patch
