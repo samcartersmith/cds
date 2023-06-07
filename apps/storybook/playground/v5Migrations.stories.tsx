41a0c5fa71f4ddd14a02becf8119c5bc3f9437cf
blair.mckee <blair.mckee@coinbase.com>
2023-06-07 14:39:30 -0500

    [trivial] chore: commented out v5 stories for removed imports. fixed merge issue in new playground routes

--- apps/storybook/playground/v5Migrations.stories.tsx
+++ apps/storybook/playground/v5Migrations.stories.tsx
@@ -1,40 +1,33 @@
 // THIS FILE IS FOR TESTING THE CDS MIGRATOR AND SHOULD NEVER BE BUILT
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 import { useCellSpacing } from '@cbhq/cds-common/hooks/useCellSpacing';
 // should update to '@cbhq-common/motion/tokens'
-// @ts-expect-error testing
-import { curves } from '@cbhq/cds-common/tokens/motion';
-// @ts-expect-error testing
-import { Animated } from '@cbhq/cds-web/animation/Animated';
+// import { curves } from '@cbhq/cds-common/tokens/motion';
+// import { Animated } from '@cbhq/cds-web/animation/Animated';
 import { IconButton } from '@cbhq/cds-web/buttons/IconButton';
-// @ts-expect-error testing
-import { CollapseArrow } from '@cbhq/cds-web/collapsible/CollapseArrow';
+// should change to Animated Caret
+// import { CollapseArrow } from '@cbhq/cds-web/collapsible/CollapseArrow';
 // should update to '@cbhq-web/navigation/Sidebar'
-// @ts-expect-error testing
-import { Sidebar } from '@cbhq/cds-web/deprecated/navigation/Sidebar';
+// import { Sidebar } from '@cbhq/cds-web/deprecated/navigation/Sidebar';
 // the import path should change to the decomped package
 import { Dropdown } from '@cbhq/cds-web/dropdown';
-// @ts-expect-error testing
-import { useIsMobile } from '@cbhq/cds-web/hooks/useIsMobile';
+// import { useIsMobile } from '@cbhq/cds-web/hooks/useIsMobile';
 // Badge import should throw warning that component was removed
 // @ts-expect-error testing
 import { Badge, Icon, LogoMark, NavigationIcon as NavIcon } from '@cbhq/cds-web/icons';
 import { Pictogram as CDSPictogram, Pictogram, SpotSquare } from '@cbhq/cds-web/illustrations';
 import { Box, Group, VStack } from '@cbhq/cds-web/layout';
 // should update to '@cbhq-web/cards/Card'
-// @ts-expect-error testing
-import { Card } from '@cbhq/cds-web/layout/Card';
-// @ts-expect-error testing
-import { none } from '@cbhq/cds-web/styles/borderRadius';
+// import { Card } from '@cbhq/cds-web/layout/Card';
+// import { none } from '@cbhq/cds-web/styles/borderRadius';
 
 export const V5MigrationsTest = () => {
   // migrated token
-  const radii = none;
+  // const radii = none;
   // removed token
-  const { global } = curves;
+  // const { global } = curves;
   // removed param
   const testDeprecatedParam = useCellSpacing({
     // @ts-expect-error testing
@@ -42,9 +35,9 @@ export const V5MigrationsTest = () => {
     offsetHorizontal: 1,
   });
   // migrated function
-  const isMobile = useIsMobile();
+  // const isMobile = useIsMobile();
   // removed function
-  const animatedTest = Animated;
+  // const animatedTest = Animated;
   return (
     <VStack>
       {/* prop value migrations */}
@@ -65,13 +58,13 @@ export const V5MigrationsTest = () => {
       {/* @ts-expect-error testing */}
       <NavIcon name="addPeople" size="s" badge={<Badge />} />
       {/* import path migrations */}
-      <Card />
+      {/* <Card /> */}
       {/* removed exports with no replacement */}
-      <Sidebar logo={<LogoMark />}>
+      {/* <Sidebar logo={<LogoMark />}>
         <li>Test</li>
-      </Sidebar>
+      </Sidebar> */}
       {/* This should get swapped out with AnimatedCaret */}
-      <CollapseArrow />
+      {/* <CollapseArrow /> */}
       {/* this should not be deprecated after package decomp migrations run */}
       <Dropdown content="test">
         <IconButton name="caretDown" />
