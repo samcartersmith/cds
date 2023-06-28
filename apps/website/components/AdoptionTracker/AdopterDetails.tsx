4f22d6b882268f8acb29e2c483c204315c2d1a7f
Emily Seibert <emily.seibert@coinbase.com>
2023-06-28 13:07:34 -0600

    POC for downloading okr planning (#2062)

--- apps/website/components/AdoptionTracker/AdopterDetails.tsx
+++ apps/website/components/AdoptionTracker/AdopterDetails.tsx
@@ -1,8 +1,9 @@
-import React, { memo, useMemo } from 'react';
+import React, { memo, useCallback, useMemo } from 'react';
 import Link from '@docusaurus/Link';
-import { IconButton } from '@cbhq/cds-web/buttons';
+import { Button, IconButton } from '@cbhq/cds-web/buttons';
 import { HStack, VStack } from '@cbhq/cds-web/layout';
 import { TextBody, TextTitle1 } from '@cbhq/cds-web/typography';
+import { getBrowserGlobals } from '@cbhq/cds-web/utils/browser';
 
 import { StatsTextStack } from ':cds-website/components/StatsTextStack';
 import { Tabs } from ':cds-website/components/Tabs';
@@ -16,7 +17,9 @@ import { useAdopterStats } from './hooks/useAdopterStats';
 import { useAdoptionPercent } from './hooks/useAdoptionPercent';
 import { useOkrPlanningComponents } from './hooks/useOkrPlanningComponents';
 import { AdopterSearchProvider } from './search/AdopterSearchProvider';
+import { getCdsRecs } from './utils/getCdsRecs';
 import { AdopterStatsBreakdown } from './AdopterStatsBreakdown';
+import { ComponentData } from './types';
 
 const AdopterTabs = memo(() => {
   const { id } = useAdopterProjectInfo();
@@ -62,19 +65,77 @@ const RouterLink = (props: React.ComponentProps<typeof Link>) => (
   <Link {...props} to="/adoption-tracker-overview" />
 );
 
+function convertArrayOfObjectsToCSV(data: ComponentData[]) {
+  const csvHeader =
+    'Custom Component Name,Suggested CDS Replacement,Call Site, Num of Instances Per Callsite';
+
+  // iterate over all custom components and for each one, iterate over all call sites and create a row
+  const csvRows = data.map((componentObject: ComponentData) => {
+    const { name, callSites: potentialCallSites } = componentObject;
+    const callSiteRecord = potentialCallSites ?? {};
+    const callSites = Object.keys(callSiteRecord);
+    const cdsRecommendations = getCdsRecs(name);
+
+    const componentRows = callSites.map((callSite: string) => {
+      return `${name},${cdsRecommendations.join('|')},${callSite},${callSiteRecord[callSite]}`;
+    });
+
+    return `${componentRows.join('\n')}`;
+  });
+
+  return `${csvHeader}\n${csvRows.join('\n')}`;
+}
+
+function downloadCSV(csvData: string, fileName: string) {
+  // Creating a Blob for having a csv file format
+  // and passing the data with type
+  const blob = new Blob([csvData], { type: 'text/csv' });
+
+  // Creating an object for downloading url
+  const url = getBrowserGlobals()?.window.URL.createObjectURL(blob);
+
+  // Creating an anchor(a) tag of HTML
+  const a = getBrowserGlobals()?.document.createElement('a');
+
+  if (a) {
+    // Passing the blob downloading url
+    a.setAttribute('href', url ?? '');
+
+    // Setting the anchor tag attribute for downloading
+    // and passing the download file name
+    a.setAttribute('download', fileName);
+
+    // Performing a download with click
+    a.click();
+  }
+}
+
 export const AdopterDetails = memo(() => {
   const { label } = useAdopterProjectInfo();
   const { latest } = useAdopterStats();
+  const okrPlanning = useOkrPlanningComponents();
+
+  const handleDownload = useCallback(() => {
+    const csvData = convertArrayOfObjectsToCSV(okrPlanning);
+    const fileName = `okr-planning-${label}.csv`;
+
+    downloadCSV(csvData, fileName);
+  }, [label, okrPlanning]);
 
   return (
     <VStack>
       <VStack>
         <HStack alignItems="flex-start" justifyContent="space-between" spacingBottom={2}>
-          <HStack alignItems="center" gap={2}>
-            <IconButton as={RouterLink} transparent name="backArrow" />
-            <TextTitle1 as="h1">{label}</TextTitle1>
-            <TextBody as="p">{`Last updated ${latest.date}`}</TextBody>
-          </HStack>
+          <VStack gap={1}>
+            <HStack alignItems="center" gap={2}>
+              <IconButton as={RouterLink} transparent name="backArrow" />
+              <TextTitle1 as="h1">{label}</TextTitle1>
+              <TextBody as="p">{`Last updated ${latest.date}`}</TextBody>
+            </HStack>
+            <Button variant="secondary" onPress={handleDownload}>
+              Download OKR Planning
+            </Button>
+          </VStack>
           <HStack width={240} gap={6}>
             <StatsTextStack label="CDS" stat={useAdoptionPercent('cds')} />
             <StatsTextStack label="Product" stat={useAdoptionPercent('presentational')} />
