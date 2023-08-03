f4637516ec36cac7f16ae02df02057d77e3f5772
Tyler Martinez <tyler.martinez@coinbase.com>
2023-08-03 08:24:06 -0700

    [DX-1097] TextInput test updates (#2125)
    * Update TextInput tests
    
    * Cleanup web tests
    
    * Update mobile tests
    
    * Remove updates to src
    
    * Bump
    
    * Move test id to correct element
    
    * Bump
    
    * Actually dont change src
    
    * Remove skipped tests

--- packages/mobile/controls/__tests__/TextInput.test.tsx
+++ packages/mobile/controls/__tests__/TextInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react-native';
+import { fireEvent, render, screen } from '@testing-library/react-native';
 
 import { TextTitle1 } from '../../typography';
 import { TextInput } from '../TextInput';
@@ -8,98 +8,182 @@ describe('TextInput', () => {
     const testID = 'textinput-id';
     render(
       <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
+        label="Text"
+        helperText="Text"
+        placeholder="Text"
+        start={<TextTitle1>Node</TextTitle1>}
+        end={<TextTitle1>Node</TextTitle1>}
+        accessibilityHint="Text"
+        accessibilityLabel="Text"
         testID={testID}
       />,
     );
     expect(screen.getByTestId(testID)).toBeAccessible();
   });
+
   it('renders a TextInput', () => {
     const testID = 'textinput-id';
+    const value = 'Example value';
     render(
       <TextInput
         accessibilityHint="Text input field"
         accessibilityLabel="Text input field"
-        testID={testID}
-      />,
-    );
-    expect(screen.getByTestId(testID)).toBeDefined();
-  });
-
-  it('renders a Label', () => {
-    const testID = 'label-id';
-    render(
-      <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        label="TextInput"
-        testIDMap={{
-          label: testID,
-        }}
-      />,
-    );
-    expect(screen.getByTestId(testID)).toBeDefined();
-  });
-
-  it('renders a HelperText', () => {
-    const testID = 'helpertext-id';
-    render(
-      <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        helperText="Helper Text"
-        testIDMap={{
-          helperText: testID,
-        }}
-      />,
-    );
-    expect(screen.getByTestId(testID)).toBeDefined();
-  });
-
-  it('renders an Input', () => {
-    const testID = 'input-id';
-    const value = 'input';
-
-    render(
-      <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        testID={testID}
         value={value}
+        onChange={jest.fn()}
+        testID={testID}
       />,
     );
     expect(screen.getByTestId(testID).props.value).toBe(value);
   });
 
-  it('renders a start node', () => {
-    const testID = 'start-id';
+  it('renders a label', () => {
+    const testID = 'label-testid';
+    const labelText = 'Example label';
     render(
       <TextInput
         accessibilityHint="Text input field"
         accessibilityLabel="Text input field"
+        label="Example label"
+        testIDMap={{
+          label: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(labelText);
+  });
+
+  it('renders label in start node when compact', () => {
+    const testID = 'start-testid';
+    const labelText = 'Example label';
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        compact
+        label="Example label"
         testIDMap={{
           start: testID,
         }}
-        start={<TextTitle1>Hello</TextTitle1>}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(labelText);
   });
 
-  it('renders an end node', () => {
-    const testID = 'end-id';
+  it('renders helper text', () => {
+    const testID = 'helpertext-testid';
+    const helperText = 'Example helper text';
     render(
       <TextInput
         accessibilityHint="Text input field"
         accessibilityLabel="Text input field"
+        compact
+        helperText={helperText}
+        testIDMap={{
+          helperText: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(helperText);
+  });
+
+  it('renders placeholder text', () => {
+    const placeholderText = 'Example placeholder text';
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        placeholder={placeholderText}
+      />,
+    );
+    expect(screen.getByPlaceholderText(placeholderText)).toBeDefined();
+  });
+
+  it('renders a start node', () => {
+    const testID = 'start-testid';
+    const startNodeText = 'Example start node';
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        start={<TextTitle1>{startNodeText}</TextTitle1>}
+        testIDMap={{
+          start: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(startNodeText);
+  });
+
+  it('renders an end node', () => {
+    const testID = 'end-testid';
+    const endNodeText = 'Example end node';
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        start={<TextTitle1>{endNodeText}</TextTitle1>}
+        testIDMap={{
+          start: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(endNodeText);
+  });
+
+  it('renders suffix in end node', () => {
+    const testID = 'end-testid';
+    const suffixText = 'Example suffix';
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        suffix={suffixText}
         testIDMap={{
           end: testID,
         }}
-        end={<TextTitle1>Hello</TextTitle1>}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(suffixText);
+  });
+
+  it('calls onChangeText when input value changes', () => {
+    const testID = 'input-testid';
+    const onChange = jest.fn();
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        onChangeText={onChange}
+        testID={testID}
+      />,
+    );
+    expect(onChange).not.toHaveBeenCalled();
+    fireEvent.changeText(screen.getByTestId(testID), 'Updated value');
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][0]).toBe('Updated value');
+  });
+
+  it('calls onFocus and onBlur when input is focused / blurred', () => {
+    const testID = 'input-testid';
+    const onFocus = jest.fn();
+    const onBlur = jest.fn();
+    render(
+      <TextInput
+        accessibilityHint="Text input field"
+        accessibilityLabel="Text input field"
+        onFocus={onFocus}
+        onBlur={onBlur}
+        testID={testID}
+      />,
+    );
+    expect(onFocus).not.toHaveBeenCalled();
+    expect(onBlur).not.toHaveBeenCalled();
+    fireEvent(screen.getByTestId(testID), 'focus');
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onBlur).not.toHaveBeenCalled();
+    fireEvent(screen.getByTestId(testID), 'blur');
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onBlur).toHaveBeenCalledTimes(1);
   });
 
   it('applies device font size scaling', () => {
--- packages/web/controls/__tests__/TextInput.test.tsx
+++ packages/web/controls/__tests__/TextInput.test.tsx
@@ -1,98 +1,170 @@
 // Disabling this because its just testing
 /* eslint-disable react-perf/jsx-no-new-object-as-prop */
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { renderA11y } from '@cbhq/cds-web-utils';
 
 import { TextTitle1 } from '../../typography';
 import { TextInput } from '../TextInput';
 
 describe('TextInput', () => {
-  it('renders a TextInput', () => {
-    const testID = 'textinput-id';
-    render(
-      <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        testID={testID}
-      />,
-    );
-    expect(screen.getByTestId(testID)).toBeDefined();
+  it('passes accessibility', async () => {
+    expect(
+      await renderA11y(
+        <TextInput
+          label="Text"
+          helperText="Text"
+          placeholder="Text"
+          start={<TextTitle1 as="h1">Node</TextTitle1>}
+          end={<TextTitle1 as="h1">Node</TextTitle1>}
+          accessibilityHint="Text"
+          accessibilityLabel="Text"
+        />,
+      ),
+    ).toHaveNoViolations();
   });
 
-  it('renders a Label', () => {
-    const testID = 'label-id';
+  it('renders an input', () => {
+    const value = 'Example value';
+    render(<TextInput value={value} onChange={jest.fn()} />);
+    expect(screen.getByRole('textbox')).toHaveValue(value);
+  });
+
+  it('renders a label', () => {
+    const testID = 'label-testid';
+    const labelText = 'Example label';
     render(
       <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        label="TextInput"
+        label="Example label"
         testIDMap={{
           label: testID,
         }}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(labelText);
   });
 
-  it('renders a HelperText', () => {
-    const testID = 'helpertext-id';
+  it('renders label in start node when compact', () => {
+    const testID = 'start-testid';
+    const labelText = 'Example label';
     render(
       <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        helperText="Helper Text"
+        compact
+        label="Example label"
+        testIDMap={{
+          start: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(labelText);
+  });
+
+  it('renders helper text', () => {
+    const testID = 'helpertext-testid';
+    const helperText = 'Example helper text';
+    render(
+      <TextInput
+        helperText={helperText}
         testIDMap={{
           helperText: testID,
         }}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(helperText);
   });
 
-  it('renders an Input', () => {
-    const testID = 'input-id';
-    const value = 'input';
-
-    render(
-      <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
-        value={value}
-        testID={testID}
-        onChange={jest.fn()}
-      />,
-    );
-    const input = screen.getByTestId(testID);
-    expect(input.getAttribute('value')).toBe(value);
+  it('renders placeholder text', () => {
+    const placeholderText = 'Example placeholder text';
+    render(<TextInput placeholder={placeholderText} />);
+    expect(screen.getByPlaceholderText(placeholderText)).toBeDefined();
   });
 
   it('renders a start node', () => {
-    const testID = 'start-id';
+    const testID = 'start-testid';
+    const startNodeText = 'Example start node';
     render(
       <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
+        start={<TextTitle1 as="h1">{startNodeText}</TextTitle1>}
         testIDMap={{
           start: testID,
         }}
-        start={<TextTitle1 as="h1">Hello</TextTitle1>}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(startNodeText);
   });
 
   it('renders an end node', () => {
-    const testID = 'end-id';
+    const testID = 'end-testid';
+    const endNodeText = 'Example end node';
     render(
       <TextInput
-        accessibilityHint="Text input field"
-        accessibilityLabel="Text input field"
+        start={<TextTitle1 as="h1">{endNodeText}</TextTitle1>}
+        testIDMap={{
+          start: testID,
+        }}
+      />,
+    );
+    expect(screen.getByTestId(testID)).toHaveTextContent(endNodeText);
+  });
+
+  it('renders suffix in end node', () => {
+    const testID = 'end-testid';
+    const suffixText = 'Example suffix';
+    render(
+      <TextInput
+        suffix={suffixText}
         testIDMap={{
           end: testID,
         }}
-        end={<TextTitle1 as="h1">Hello</TextTitle1>}
       />,
     );
-    expect(screen.getByTestId(testID)).toBeDefined();
+    expect(screen.getByTestId(testID)).toHaveTextContent(suffixText);
+  });
+
+  it('calls onChange when input value changes', () => {
+    const onChange = jest.fn();
+    render(<TextInput onChange={onChange} />);
+    expect(onChange).not.toHaveBeenCalled();
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: {
+        value: 'Updated value',
+      },
+    });
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange.mock.calls[0][0].target.value).toBe('Updated value');
+  });
+
+  it('calls onFocus and onBlur when input is focused / blurred', () => {
+    const onFocus = jest.fn();
+    const onBlur = jest.fn();
+    render(<TextInput onFocus={onFocus} onBlur={onBlur} />);
+    expect(onFocus).not.toHaveBeenCalled();
+    expect(onBlur).not.toHaveBeenCalled();
+    fireEvent.focus(screen.getByRole('textbox'));
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onBlur).not.toHaveBeenCalled();
+    fireEvent.blur(screen.getByRole('textbox'));
+    expect(onFocus).toHaveBeenCalledTimes(1);
+    expect(onBlur).toHaveBeenCalledTimes(1);
+  });
+
+  it('focuses input when start node is pressed', () => {
+    const onFocus = jest.fn();
+    const startNodeText = 'Start';
+    render(
+      <TextInput onFocus={onFocus} start={<TextTitle1 as="h1">{startNodeText}</TextTitle1>} />,
+    );
+    expect(onFocus).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByText(startNodeText));
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  });
+
+  it('focuses input when end node is pressed', () => {
+    const onFocus = jest.fn();
+    const endNodeText = 'End';
+    render(<TextInput onFocus={onFocus} end={<TextTitle1 as="h1">{endNodeText}</TextTitle1>} />);
+    expect(onFocus).not.toHaveBeenCalled();
+    fireEvent.click(screen.getByText(endNodeText));
+    expect(onFocus).toHaveBeenCalledTimes(1);
   });
 
   it('Generates accessible id for screen reader to read label if label exists', () => {
