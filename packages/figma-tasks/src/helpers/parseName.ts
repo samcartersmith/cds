3eb1985404d648a5c33a476d27ca49a4fde48626
Emily Seibert <emily.seibert@coinbase.com>
2023-08-17 19:31:21 -0600

    [DX-735] fix(Icon/Illustrations): Throw error if names are incorrectly formatted (#2166)
    * Add camelCase check into item creation for icons & illustrations
    
    * fix formatting & remove console log
    
    * Move logic to parseName
    
    * fix typo

--- packages/figma-tasks/src/helpers/parseName.ts
+++ packages/figma-tasks/src/helpers/parseName.ts
@@ -8,9 +8,17 @@ export function parseName(node: { document: { name: string } }) {
   if (parts.length < 2) {
     throw new Error(`${node.document.name} needs to use the format [type]/[name] format`);
   }
+
   const [originalType, originalName] = parts;
   const type = camelCase(originalType);
   const name = originalName;
+
+  // Regular expression to match camel case pattern
+  const camelCasePattern = /^[a-z][a-zA-Z0-9]*$/;
+  if (!camelCasePattern.test(name)) {
+    throw new Error(`Name is not in camelCase: ${name}. Figma file should be updated.`);
+  }
+
   return {
     type,
     name,
