72ffa91cc7e88e6583550c27711639f35d31a4cc
Blair McKee <blair.mckee@coinbase.com>
2023-10-25 10:36:36 -0500

    [DX-2193] internal: cleaned up dangerJS warnings (#2365)
    * [trivial] internal: removed frozen component warning because deprecations covers them already
    
    * [trivial] internal: updated deprecation warnings
    
    * [trivial] internal: removed large PR check

--- libs/dangerfiles/dangerfile.ts
+++ libs/dangerfiles/dangerfile.ts
@@ -5,12 +5,7 @@ import chainsmoker from 'danger/distribution/commands/utils/chainsmoker';
 import checkForBreakingChanges from './breakingChanges/checkForBreakingChanges';
 import checkFilesForRemovedExports from './breakingChanges/exports';
 import checkFilesForRemovedParams from './breakingChanges/params';
-import {
-  cdsPackagesPatterns,
-  componentPatterns,
-  fileIgnorePatterns,
-  frozenComponentPatterns,
-} from './patterns';
+import { cdsPackagesPatterns, componentPatterns, fileIgnorePatterns } from './patterns';
 import { findAssociatedFilesWithoutSuffix, findPatternInGitDiffs, getModifiedFiles } from './utils';
 
 // Useful for running locally with yarn danger local --dangerfile libs/dangerfiles/dangerfile.ts
@@ -28,15 +23,12 @@ danger.git.fileMatch = chainsmoker({
   deleted: danger.git.deleted_files,
 });
 
-const bigPRThreshold = 600;
-
 const pr = danger.github?.pr || MOCK_PR;
 const bodyAndTitle = (pr.body + pr.title).toLowerCase();
 
 // Custom modifiers for people submitting PRs to be able to say "skip this"
 const acceptedNoTests = bodyAndTitle.includes('#skip_tests');
 const acceptedNoStories = bodyAndTitle.includes('#skip_stories');
-const acceptedOverrideFrozenComponent = bodyAndTitle.includes('#skip_frozen_component');
 const acceptedOverrideDeprecation = bodyAndTitle.includes('#skip_deprecations');
 
 const nonTestTsFiles = danger.git.fileMatch(
@@ -56,7 +48,6 @@ const storyFiles = danger.git.fileMatch(
   `packages/${process.env.NX_PROJECT_NAME}/__stories__/**.(ts|tsx)`,
 );
 
-const frozenComponentFiles = danger.git.fileMatch(...frozenComponentPatterns);
 const componentFiles = danger.git.fileMatch(...componentPatterns);
 const cdsPackagesFiles = danger.git.fileMatch(...cdsPackagesPatterns);
 
@@ -135,27 +126,6 @@ if (componentFiles.edited) {
   );
 }
 
-// Warn against modifying a frozen component
-if (frozenComponentFiles.edited && !acceptedOverrideFrozenComponent) {
-  fail(
-    [
-      "## 🥶 Don't modify frozen components!",
-      `You have edited file(s) belonging to a **frozen component**. You must qualify for an exception and add \`#skip_frozen_component\` in the PR description to skip this check.\n`,
-      frozenComponentFiles
-        .getKeyedPaths()
-        .edited.map((file) => `- ${file}`)
-        .join('\n'),
-    ].join('\n'),
-  );
-}
-
-// Encourage smaller PRs
-if (pr.additions + pr.deletions > bigPRThreshold) {
-  message(
-    'Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR will helps faster, easier review.',
-  );
-}
-
 // Look for deprecated in each file that has been touched
 void (async () => {
   const deprecations = await findPatternInGitDiffs({
@@ -168,7 +138,7 @@ void (async () => {
   if (deprecations.modified.length && !acceptedOverrideDeprecation) {
     fail(
       [
-        '## 🛑 Do not modify Deprecations!',
+        '## 🤨 Did you modify a deprecated component?',
         `You have edited file(s) that contain a deprecation. Make sure the modification you made is not to a deprecated component, token, hook, parameter, prop, or function. Please refer to [go/cds-deprecations](https://cds.cbhq.net/resources/deprecations) for the full list of deprecations.`,
         deprecations.modified.map((file) => `- ${file}`).join('\n'),
       ].join('\n'),
@@ -179,20 +149,9 @@ void (async () => {
   if (deprecations.created.length && !acceptedOverrideDeprecation) {
     warn(
       [
-        '## 👀 Looks like you marked something as Deprecated!',
-        `You have added file(s) that contain a deprecation. Nice! Make sure there is a plan in place to announce this change and delete deprecated code in the next major release. Please refer to [go/cds-deprecations](https://cds.cbhq.net/resources/deprecations) for the full list of deprecations.`,
+        '## 🛑 Looks like you marked something as Deprecated! Did you add a migrator script for it? ',
+        `Deprecations are future breaking changes! You'll need to follow the [CDS Deprecation Process](https://github.cbhq.net/frontend/cds/blob/master/docs/conventions/deprecations.md) and write migration scripts in for your deprecation in the next major release. `,
         deprecations.created.map((file) => `- ${file}`).join('\n'),
-      ].join('\n'),
-    );
-  }
-
-  // Celebrate removing deprecated components
-  if (deprecations.deleted.length) {
-    message(
-      [
-        '## 🎉 Thanks for removing deprecated code!',
-        `You removed file(s) that contain a deprecation. This calls for a celebration!`,
-        deprecations.deleted.map((file) => `- ${file}`).join('\n'),
       ].join('\n'),
     );
   }
