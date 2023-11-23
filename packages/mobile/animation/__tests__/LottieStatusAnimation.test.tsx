3cb487b7ff2bb684544789ab110202360f0195a5
Ben Cheung <ben.cheung@coinbase.com>
2023-11-23 16:04:36 -0500

    [DX-2266] Enhance LottieStatusAnimation tests (#2474)
    * add testing for onfinish for lottiestatusanimation
    
    * increase test coverage to web
    
    * added additional unit test to mobile

--- packages/mobile/animation/__tests__/LottieStatusAnimation.test.tsx
+++ packages/mobile/animation/__tests__/LottieStatusAnimation.test.tsx
@@ -1,7 +1,38 @@
-import { render, screen } from '@testing-library/react-native';
+import { render, screen, waitFor } from '@testing-library/react-native';
 import LottieView from 'lottie-react-native';
+import {
+  LottieStatusAnimationProps,
+  LottieStatusAnimationType,
+  useStatusAnimationPoller,
+} from '@cbhq/cds-common';
 
 import { LottieStatusAnimation } from '../LottieStatusAnimation';
+
+type UseStatusAnimationPollerParams = {
+  onFinish?: () => void;
+};
+
+type UseStatusAnimationPollerReturn = ReturnType<typeof useStatusAnimationPoller>;
+
+type CdsCommonMockTypes = {
+  useStatusAnimationPoller: typeof useStatusAnimationPoller;
+};
+
+jest.mock('@cbhq/cds-common', () => {
+  const actual: CdsCommonMockTypes = jest.requireActual('@cbhq/cds-common');
+
+  return {
+    ...actual,
+    useStatusAnimationPoller: jest
+      .fn<UseStatusAnimationPollerReturn, [UseStatusAnimationPollerParams]>()
+      .mockImplementation(({ onFinish }) => {
+        setTimeout(() => {
+          if (onFinish) onFinish();
+        }, 1000);
+        return jest.fn();
+      }),
+  };
+});
 
 describe('LottieStatusAnimation', () => {
   it('renders a LottieStatusAnimation', () => {
@@ -9,5 +40,35 @@ describe('LottieStatusAnimation', () => {
 
     expect(screen.UNSAFE_queryAllByType(LottieView)).toHaveLength(1);
     expect(screen.getByTestId('lottie-status-animation-test')).toBeTruthy();
+  });
+
+  it('renders a LottieStatusAnimation and calls onFinish', async () => {
+    const mockOnFinish = jest.fn();
+
+    render(
+      <LottieStatusAnimation
+        height="100"
+        onFinish={mockOnFinish}
+        testID="lottie-status-animation-test"
+      />,
+    );
+
+    expect(screen.getByTestId('lottie-status-animation-test')).toBeTruthy();
+    await waitFor(() => expect(mockOnFinish).toHaveBeenCalled(), { timeout: 1500 });
+  });
+
+  it('renders LottieStatusAnimation with different status values', () => {
+    const testStatuses: LottieStatusAnimationType[] = ['loading', 'success', 'failure', 'pending'];
+
+    testStatuses.forEach((status) => {
+      const props: LottieStatusAnimationProps = {
+        status,
+        testID: `lottie-status-animation-${status}`,
+        height: '100',
+      };
+
+      render(<LottieStatusAnimation {...props} />);
+      expect(screen.getByTestId(`lottie-status-animation-${status}`)).toBeTruthy();
+    });
   });
 });
--- packages/web/animation/__tests__/LottieStatusAnimation.test.tsx
+++ packages/web/animation/__tests__/LottieStatusAnimation.test.tsx
@@ -1,10 +1,69 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { LottieStatusAnimationProps, LottieStatusAnimationType } from '@cbhq/cds-common';
 
 import { LottieStatusAnimation } from '../LottieStatusAnimation';
 
-describe('Lottie', () => {
+type StatusAnimationPollerParams = {
+  onFinish?: () => void;
+};
+
+jest.mock('@cbhq/cds-common', () => ({
+  useStatusAnimationPoller: jest
+    .fn()
+    .mockImplementation(({ onFinish }: StatusAnimationPollerParams) => {
+      setTimeout(() => {
+        if (onFinish) onFinish();
+      }, 1000);
+      return jest.fn();
+    }),
+}));
+
+jest.mock('../Lottie', () => ({
+  Lottie: jest
+    .fn()
+    .mockImplementation(
+      ({ onAnimationFinish, testID }: { onAnimationFinish?: () => void; testID?: string }) => {
+        if (onAnimationFinish) {
+          setTimeout(onAnimationFinish, 1000);
+        }
+        return <div data-testid={testID} />;
+      },
+    ),
+}));
+
+describe('LottieStatusAnimation', () => {
   it('renders LottieStatusAnimation', () => {
     render(<LottieStatusAnimation height="100" testID="lottie-status-animation-test" />);
     expect(screen.getByTestId('lottie-status-animation-test')).toBeTruthy();
+  });
+
+  it('renders LottieStatusAnimation and calls onFinish', async () => {
+    const onFinish = jest.fn();
+    render(
+      <LottieStatusAnimation
+        height="100"
+        onFinish={onFinish}
+        testID="lottie-status-animation-test"
+      />,
+    );
+
+    expect(screen.getByTestId('lottie-status-animation-test')).toBeTruthy();
+    await waitFor(() => expect(onFinish).toHaveBeenCalled(), { timeout: 1500 });
+  });
+
+  it('renders with different status values', () => {
+    const testStatuses: LottieStatusAnimationType[] = ['loading', 'success', 'failure', 'pending'];
+
+    testStatuses.forEach((status) => {
+      const props: LottieStatusAnimationProps = {
+        status,
+        testID: `lottie-status-animation-${status}`,
+        height: '100',
+      };
+
+      render(<LottieStatusAnimation {...props} />);
+      expect(screen.getByTestId(`lottie-status-animation-${status}`)).toBeTruthy();
+    });
   });
 });
