aafa4ade789e5209ecbce6eb7f259b8a058ac996
Cody Nova <cody.nova@coinbase.com>
2025-02-19 16:00:36 -0600

    Add changelogs for v8 (#3621)

---
+++ rewrite-alpha.mjs
@@ -0,0 +1,25 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { globSync } from 'glob';
+import { fileURLToPath } from 'node:url';
+
+const root = path.dirname(fileURLToPath(import.meta.url));
+
+const pkg = process.argv[2];
+
+if (!pkg) throw Error('Missing package argument!!!!!!!!!');
+
+const dependencies = ['cds-common', 'cds-mobile', 'cds-mobile-visualization', 'cds-lottie-files'];
+
+const dts = globSync(`${root}/packages/${pkg}/dts/**/*`, { nodir: true });
+const esm = globSync(`${root}/packages/${pkg}/esm/**/*`, { nodir: true });
+
+const pkgJson = `${root}/packages/${pkg}/package.json`;
+
+for (const filepath of [pkgJson, ...dts, ...esm]) {
+  let newContents = fs.readFileSync(filepath, 'utf-8');
+  for (const dep of dependencies) {
+    newContents = newContents.replaceAll(`@cbhq/${dep}2`, `@cbhq/${dep}`);
+  }
+  fs.writeFileSync(filepath, newContents, { encoding: 'utf-8' });
+}
