a174975090636165f21ab47080aabc4e10bb7d8b
Cody Nova <cody.nova@coinbase.com>
2025-06-20 07:44:50 -0500

    Update web Icon stories to include all icons (#4088)

--- packages/web/src/icons/__stories__/IconSheet.tsx
+++ packages/web/src/icons/__stories__/IconSheet.tsx
@@ -2,7 +2,8 @@ import React, { memo } from 'react';
 import { type IconName, type IconSize } from '@cbhq/cds-common';
 import { sortNamesByOldOrder } from '@cbhq/cds-common/internal/utils/sortIconsForVisReg';
 import { UiIconName } from '@cbhq/cds-icons';
-import names from '@cbhq/cds-icons/__generated__/ui/data/names';
+import navNames from '@cbhq/cds-icons/__generated__/nav/data/names';
+import uiNames from '@cbhq/cds-icons/__generated__/ui/data/names';
 import namesOld from '@cbhq/cds-icons/__generated__/ui/data/names-old';
 
 import { useTheme } from '../../hooks/useTheme';
@@ -10,13 +11,15 @@ import { HStack, VStack } from '../../layout';
 import { Text } from '../../typography/Text';
 import { getIconSourceSize, Icon } from '../Icon';
 
-type SvgFromFigmaProps = { name: IconName; size: IconSize };
+type SvgFromFigmaProps = { name: IconName; size: IconSize; type: 'ui' | 'nav' };
 
-function SvgFromFigma({ name, size }: SvgFromFigmaProps) {
+function SvgFromFigma({ name, size, type }: SvgFromFigmaProps) {
   const theme = useTheme();
   const iconSize = theme.iconSize[size];
   const sourceSize = getIconSourceSize(iconSize);
-  const svgPath = `@cbhq/cds-icons/__generated__/ui/svg/ui-${name}-${sourceSize}.svg`;
+  const svgPath = `@cbhq/cds-icons/__generated__/${type}/svg/${type}-${name}-${sourceSize}${
+    type === 'nav' ? '-inactive' : ''
+  }.svg`;
   return <img alt={name} height={iconSize} src={svgPath} width={iconSize} />;
 }
 
@@ -35,24 +38,41 @@ type IconSheetProps = {
 };
 
 export const IconSheet = memo(function IconSheet({ startIndex, endIndex }: IconSheetProps) {
-  const sortedNames = sortNamesByOldOrder(namesOld, names) as UiIconName[]; // Use sorted names
+  const uiNamesData = uiNames.map((name) => ({ name, type: 'ui' as const }));
+  const navNamesData = navNames.map((name) => ({ name, type: 'nav' as const }));
+  const sortedNames = [...uiNamesData, ...navNamesData];
 
   return (
     <VStack gap={2}>
       <DisclaimerText />
       <HStack flexWrap="wrap" gap={2} paddingBottom={2}>
-        {sortedNames.slice(startIndex, endIndex).map((name) => {
+        {sortedNames.slice(startIndex, endIndex).map(({ name, type }) => {
           return (
             <HStack key={`icon-wrapper-${name}`} flexWrap="wrap" gap={2}>
               <VStack gap={2}>
                 <HStack alignItems="center" gap={2}>
                   {(['xs', 's', 'm', 'l'] as const).map((size) => {
-                    return <Icon key={`icon-${size}`} color="fg" name={name} size={size} />;
+                    return (
+                      <Icon
+                        key={`icon-${size}`}
+                        color="fg"
+                        iconType={type === 'nav' ? 'nav' : undefined}
+                        name={name}
+                        size={size}
+                      />
+                    );
                   })}
                 </HStack>
                 <HStack alignItems="center" gap={2}>
                   {(['xs', 's', 'm', 'l'] as const).map((size) => {
-                    return <SvgFromFigma key={`figma-icon-${size}`} name={name} size={size} />;
+                    return (
+                      <SvgFromFigma
+                        key={`figma-icon-${size}`}
+                        name={name}
+                        size={size}
+                        type={type}
+                      />
+                    );
                   })}
                 </HStack>
               </VStack>
