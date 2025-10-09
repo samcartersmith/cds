64a56da97e999b86785d2f942ba33dad2e7c4782
Cody Nova <cody.nova@coinbase.com>
2025-10-09 13:05:37 -0500

    feat: Update sync-icons task and icon release docs (#4178)
    * Update sync-icons task and icon release docs
    
    * Update error handling
    
    * Fix error messages
    
    * Format

--- docs/release/icons.md
+++ docs/release/icons.md
@@ -1,3 +1,80 @@
 ## Release Icons
 
-See icons package [README.md](../../packages/icons/README.md)
+**WARNING: FOLLOW THESE INSTRUCTIONS EXACTLY. Use a fresh clone of the repos EVERY TIME. Copy and paste these commands directly into your terminal, editing them with the current date as necessary. DO NOT MESS AROUND.**
+
+**IMPORTANT: If any icons are renamed or deleted, this is a BREAKING CHANGE. This MUST be published with an accompanying major version bump, migration guide, and a migrator script.**
+
+1. Make sure any existing sync repo names have been deleted
+
+```sh
+rm -rf temp-oss-cds temp-frontend-cds
+```
+
+2. Clone the github.com/coinbase/cds repo with this name
+
+```sh
+git clone git@github.com:coinbase/cds.git temp-oss-cds
+```
+
+3. Clone the github.cbhq.net/frontend/cds repo as a sibling with this name
+
+```sh
+git clone git@github.cbhq.net:frontend/cds.git temp-frontend-cds
+```
+
+4. Navigate into the `temp-oss-cds` repo and create a new branch with this name pattern
+
+```sh
+cd temp-oss-cds
+git checkout -b icons/YYYY-MM-DD
+```
+
+5. Navigate into the `temp-frontend-cds` repo and run the icon sync script
+
+```sh
+cd ../temp-frontend-cds
+yarn nx run icon-tasks:sync-icons
+```
+
+6. Navigate back into the `temp-oss-cds` repo and commit the changes with this message pattern, then push the branch
+
+```sh
+cd ../temp-oss-cds
+git add .
+git commit -m 'feat: Publish icons YYYY/MM/DD'
+git push origin icons/YYYY-MM-DD
+```
+
+7. Open a PR in the github.com/coinbase/cds repo. Title the PR exactly the same as the commit message: `feat: Publish icons YYYY/MM/DD`. Take note of the PR number for the next step
+
+8. Still in the `temp-oss-cds` repo, update the icons package changelog by completing the prompts as shown below
+
+```sh
+yarn changelog
+```
+
+> - Type of change?: Choose "Breaking" for breaking changes, otherwise choose "Update"
+> - Changelog message?: "Publish icons YYYY/MM/DD"
+> - PR number?: Copy/paste your PR number
+> - Skip the rest (press enter to use defaults)
+
+9. Commit and push the changes to your PR
+
+```sh
+git add .
+git commit -m 'Update changelog'
+git push origin icons/YYYY-MM-DD
+```
+
+10. DM the icons DRI on Slack and share direct links to:
+
+- the icon changelog in your PR (example)
+- the Web Visual Regression results in Percy (example)
+
+You can get the Percy link from the GitHub Actions "Visreg Web" job on your PR
+
+11. Carefully review the two links you shared with the icons DRI. Does the changelog look correct? Do the visual regression results look correct?
+
+**IMPORTANT: Breaking change releases are a big deal. They should be performed extremely rarely, and should ALWAYS be accompanied by a migration plan. You are responsible for any breaking changes that you release.**
+
+12. DO NOT MERGE until the icons DRI has carefully reviewed and signed off on the changelog and the visual regression test results.
--- libs/icon-tasks/src/sync-icons/config.ts
+++ libs/icon-tasks/src/sync-icons/config.ts
@@ -1,3 +1,4 @@
+import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
 
@@ -25,16 +26,58 @@ type SyncIconsConfig = {
 const MONOREPO_ROOT = process.env.PROJECT_CWD ?? process.env.NX_MONOREPO_ROOT;
 if (!MONOREPO_ROOT) throw Error('MONOREPO_ROOT is undefined');
 
-const CDSNEXT_ROOT = path.resolve(MONOREPO_ROOT, '../cds-next');
-if (!fs.existsSync(CDSNEXT_ROOT)) throw Error('CDSNEXT_ROOT does not exist');
+const error = (message: string, ...args: unknown[]) => {
+  console.error('ERROR: ', message, ...args);
+  console.log('');
+  process.exit(1);
+};
+
+const CDS_OSS_ROOT = path.resolve(MONOREPO_ROOT, '../temp-cds-oss');
+if (!fs.existsSync(CDS_OSS_ROOT))
+  error(`The "temp-cds-oss" directory does not exist at filepath: "${CDS_OSS_ROOT}"`);
+
+const validateFreshRepo = (dirname: string) => {
+  const repoName = path.basename(dirname);
+  const exec = (command: string) => execSync(command, { cwd: dirname }).toString().trim();
+
+  const gitBranch = exec('git branch --show-current');
+  if (gitBranch !== 'master') error(`The "${repoName}" repo is not on the "master" branch`);
+  const gitStatus = exec('git status --short');
+  if (gitStatus.length > 0) error(`The "${repoName}" repo is not clean`);
+  const gitDiff = exec('git diff --exit-code');
+  if (gitDiff.length > 0) error(`The "${repoName}" repo has changes`);
+  try {
+    exec('git remote show origin');
+  } catch (err) {
+    error(`There was an error checking the "origin" remote for the "${repoName}" repo:`, err);
+  }
+  try {
+    exec('git fetch origin');
+  } catch (err) {
+    error(`There was an error fetching the "origin" remote for the "${repoName}" repo:`, err);
+  }
+  const gitBranchChanges = exec('git log origin/master..master');
+  if (gitBranchChanges.length > 0)
+    error(
+      `The "${repoName}" repo has changes on the local master branch that are not on the origin/master branch`,
+    );
+  const gitOriginChanges = exec('git log master..origin/master');
+  if (gitOriginChanges.length > 0)
+    error(
+      `The "${repoName}" repo has changes on the origin/master branch that are not on the local master branch`,
+    );
+};
+
+validateFreshRepo(MONOREPO_ROOT);
+validateFreshRepo(CDS_OSS_ROOT);
 
 export const config: SyncIconsConfig = {
   figmaFileId: '1J3XC4iA2xRzlnC3y0pl1N',
-  changelogPath: path.resolve(CDSNEXT_ROOT, 'packages/icons/CHANGELOG.md'),
-  manifestPath: path.resolve(CDSNEXT_ROOT, 'packages/icons/manifest.json'),
-  outputSvgPath: path.resolve(CDSNEXT_ROOT, 'packages/icons/src/svgs'),
-  outputDataPath: path.resolve(CDSNEXT_ROOT, 'packages/icons/src'),
-  outputFontPath: path.resolve(CDSNEXT_ROOT, 'packages/icons/src/fonts'),
+  changelogPath: path.resolve(CDS_OSS_ROOT, 'packages/icons/CHANGELOG.md'),
+  manifestPath: path.resolve(CDS_OSS_ROOT, 'packages/icons/manifest.json'),
+  outputSvgPath: path.resolve(CDS_OSS_ROOT, 'packages/icons/src/svgs'),
+  outputDataPath: path.resolve(CDS_OSS_ROOT, 'packages/icons/src'),
+  outputFontPath: path.resolve(CDS_OSS_ROOT, 'packages/icons/src/fonts'),
   outputFontName: 'CoinbaseIcons',
   outputFontCssFileName: 'icon-font',
   syncAll: false,
